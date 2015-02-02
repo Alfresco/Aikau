8dbc6fdac1556aa9eff83484861c38b876b20294
Martin Doyle <mdoyle@mdoyle-MBP-eng-012015.local>
2015-02-02 10:22:18 +0000

    AKU-71: Update InlineEditPropertyTest.js and duplicate/modify to create InlineEditPropertyLinkTest.js

---
+++ aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/renderers/InlineEditPropertyLink.get.js
@@ -0,0 +1,78 @@
+model.jsonModel = {
+   services: [
+      {
+         name: "alfresco/services/LoggingService",
+         config: {
+            loggingPreferences: {
+               enabled: true,
+               all: true
+            }
+         }
+      },
+      "aikauTesting/mockservices/MockCrudService",
+      "alfresco/services/ErrorReporter"
+   ],
+   widgets:[
+      {
+         name: "alfresco/documentlibrary/views/AlfDocumentListView",
+         config: {
+            id: "LIST",
+            currentData: {
+               items: [
+                  {
+                     name: "Test",
+                     option: "1"
+                  }
+               ]
+            },
+            widgets:[
+               {
+                  name: "alfresco/documentlibrary/views/layouts/Row",
+                  config: {
+                     widgets: [
+                        {
+                           name: "alfresco/documentlibrary/views/layouts/Cell",
+                           config: {
+                              widgets: [
+                                 {
+                                    id: "INLINE_EDIT",
+                                    name: "alfresco/renderers/InlineEditPropertyLink",
+                                    config: {
+                                       propertyToRender: "name",
+                                       linkPublishTopic: "TEST_PROPERTY_LINK_CLICK",
+                                       linkPublishPayload: {},
+                                       publishTopic: "ALF_CRUD_UPDATE",
+                                       publishPayloadType: "PROCESS",
+                                       publishPayloadModifiers: ["processCurrentItemTokens"],
+                                       publishPayloadItemMixin: true,
+                                       publishPayload: {
+                                          url: "api/solr/facet-config/{name}"
+                                       },
+                                       hiddenDataRules: [
+                                          {
+                                             name: "hiddenData",
+                                             rulePassValue: "hidden_update",
+                                             ruleFailValue: "",
+                                             is: ["New"]
+                                          }
+                                       ]
+                                    }
+                                 }
+                              ]
+                           }
+                        }
+                     ]
+                  }
+               }
+            ]
+
+         }
+      },
+      {
+         name: "alfresco/logging/SubscriptionLog"
+      },
+      {
+         name: "aikauTesting/TestCoverageResults"
+      }
+   ]
+};
