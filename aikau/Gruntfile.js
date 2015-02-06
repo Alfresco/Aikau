c16d140fe5b264793ced9203913f3ea982f138c8
Martin Doyle <martin.doyle@alfresco.com>
2015-02-06 16:13:25 +0000

    AKU-84: Tidy Grunt files (fix JSHint errors in Gruntfile)

--- aikau/Gruntfile.js
+++ aikau/Gruntfile.js
@@ -1,43 +1,28 @@
-(function () {
-   'use strict';
-
+(function() {
 
    // Keep all paths in one place
    var alf = {
-      cssFiles: 'src/main/resources/**/*.css',
-      jsdocFiles: 'src/main/resources/alfresco',
-      jsdocReadme: 'README.md',
-      jsInstFiles: 'src/main/resources/alfrescoInst',
-      jsFiles: ['src/main/resources/alfresco/**/*.js'],
-      testFiles: ['tests/alfresco/**'],
-      htmlFiles: ['src/main/resources/alfresco/**/*.html'],
-      xmlFiles: ['/**/*.xml'],
-      testResourcesDir: 'src/test/resources',
-      vagrantDir: 'src/test/vagrant',
-      nodeBinDir: 'node_modules/.bin/',
+      cssFiles: "src/main/resources/**/*.css",
+      jsdocFiles: "src/main/resources/alfresco",
+      jsdocReadme: "README.md",
+      jsInstFiles: "src/main/resources/alfrescoInst",
+      jsFiles: ["src/main/resources/alfresco/**/*.js"],
+      testFiles: ["tests/alfresco/**"],
+      htmlFiles: ["src/main/resources/alfresco/**/*.html"],
+      xmlFiles: ["/**/*.xml"],
+      testResourcesDir: "src/test/resources",
+      vagrantDir: "src/test/vagrant",
+      nodeBinDir: "node_modules/.bin/",
       coverageDirectory: "code-coverage-reports",
-      rootDir: './',
-      codeDir: './',
-      docsDir: 'docs',
-      alfWidgetsList: 'alfresco_widgets.json',
-      requireEverythingTemplate: 'RequireEverything.template.js',
-      requireEverythingWidget: 'src/test/resources/testApp/js/aikau/testing/RequireEverything.js',
-      requireEverythingWidgetsPrefix: 'src/main/resources/',
-      requireEverythingWidgetsSuffix: '.js',
+      rootDir: "./",
+      codeDir: "./",
+      docsDir: "docs",
+      alfWidgetsList: "alfresco_widgets.json",
+      requireEverythingTemplate: "RequireEverything.template.js",
+      requireEverythingWidget: "src/test/resources/testApp/js/aikau/testing/RequireEverything.js",
+      requireEverythingWidgetsPrefix: "src/main/resources/",
+      requireEverythingWidgetsSuffix: ".js",
       requireEverythingExclusions: [
-         'alfresco/forms/controls/AceEditor',
-         'alfresco/forms/controls/CodeMirrorEditor',
-         "alfresco/charts/ccc/BarChart",
-   "alfresco/charts/ccc/Chart",
-   "alfresco/charts/ccc/ChartsView",
-   "alfresco/charts/ccc/DonutChart",
-   "alfresco/charts/ccc/PieChart",
-         "alfresco/editors/TinyMCE",
-         "alfresco/external-doc",
-         "alfresco/reports/Report",
-         "alfresco/reports/SiteContentReport",
-         "alfresco/reports/TopSiteContributorReport",
-         'aikauTesting/mockservices/SiteMockXhr',
          "aikauTesting/mockservices/CreateContentMockXhr",
          "aikauTesting/mockservices/DocumentLibraryMockXhr",
          "aikauTesting/mockservices/HeaderMockXhr",
@@ -46,26 +31,37 @@
          "aikauTesting/mockservices/SearchMockXhr",
          "aikauTesting/mockservices/SearchScrollMockXhr",
          "aikauTesting/mockservices/SiteMockXhr",
+         "aikauTesting/mockservices/SiteMockXhr",
          "aikauTesting/mockservices/UploadMockXhr",
          "aikauTesting/mockservices/UserMockXhrBad",
-         "aikauTesting/mockservices/UserMockXhrGood"
+         "aikauTesting/mockservices/UserMockXhrGood",
+         "alfresco/charts/ccc/BarChart",
+         "alfresco/charts/ccc/Chart",
+         "alfresco/charts/ccc/ChartsView",
+         "alfresco/charts/ccc/DonutChart",
+         "alfresco/charts/ccc/PieChart",
+         "alfresco/editors/TinyMCE",
+         "alfresco/external-doc",
+         "alfresco/forms/controls/AceEditor",
+         "alfresco/forms/controls/CodeMirrorEditor",
+         "alfresco/reports/Report",
+         "alfresco/reports/SiteContentReport",
+         "alfresco/reports/TopSiteContributorReport"
       ],
-
-      testModelFiles: ['src/test/resources/alfresco/**/*.json'],
-      alfTestModels: 'alfresco_model_files.json'
-
+      testModelFiles: ["src/test/resources/alfresco/**/*.json"],
+      alfTestModels: "alfresco_model_files.json"
    };
 
    module.exports = function(grunt) {
 
       // Load all grunt modules
       // @see: https://github.com/sindresorhus/load-grunt-tasks
-      require('load-grunt-tasks')(grunt);
+      require("load-grunt-tasks")(grunt);
 
-      var _ = require('lodash'), // Add the lodash util library
+      var _ = require("lodash"), // Add the lodash util library
          extend = _.extend,
          config = {},
-         path = './src/grunt/';
+         path = "./src/grunt/";
 
       /* Load the external config.
        * External config can call grunt methods, access the alf object and
@@ -74,9 +70,13 @@
        * all http config should go into the http.js file.
        * TODO: Fix this limitation?
        */
-      require('glob').sync('*', {cwd: path}).forEach(function(configFile) {
-         config = extend(config, require(path + configFile)(grunt, alf));
-      });
+      require("glob")
+         .sync("*", {
+            cwd: path
+         })
+         .forEach(function(configFile) {
+            config = extend(config, require(path + configFile)(grunt, alf));
+         });
 
       grunt.initConfig(config);
 
@@ -87,12 +87,12 @@
        */
 
       // By default we just test
-      grunt.registerTask('default', [ 'test' ]);
+      grunt.registerTask("default", ["test"]);
 
       // Register JSDocs alias:
-      grunt.registerTask('jsdoc', [
-         'copy:customJsDocTemplate',
-         'shell:jsdoc'
+      grunt.registerTask("jsdoc", [
+         "copy:customJsDocTemplate",
+         "shell:jsdoc"
       ]);
 
       // TODO: Rationalise these once we've got a workflow sorted.
@@ -105,91 +105,91 @@
       //      grunt si: incremental build, exploded deploy
 
       // Dev
-      grunt.registerTask('d', [
-         'shell:startRepoExistingBuild', // Don't do a repo build, just start an existing one.
-         'shell:startShare',
-         'dev'
+      grunt.registerTask("d", [
+         "shell:startRepoExistingBuild", // Don't do a repo build, just start an existing one.
+         "shell:startShare",
+         "dev"
       ]);
 
       // Dev - as per dev, but with a clean update first.
-      grunt.registerTask('d-cup', [
-         'cup',
-         'dev'
+      grunt.registerTask("d-cup", [
+         "cup",
+         "dev"
       ]);
 
-      grunt.registerTask('dev', [
-         'watch'
+      grunt.registerTask("dev", [
+         "watch"
       ]);
 
       // Shortcuts to incremental builds of Share/Repo
-      grunt.registerTask('r', [
-         'shell:killRepo',
-         'shell:startRepo'
+      grunt.registerTask("r", [
+         "shell:killRepo",
+         "shell:startRepo"
       ]);
-      grunt.registerTask('s', [
-         'shell:se',
-         'shell:resetCaches'
+      grunt.registerTask("s", [
+         "shell:se",
+         "shell:resetCaches"
       ]);
-      grunt.registerTask('si', [
-         'shell:killShare',
-         'shell:startShareInc'
+      grunt.registerTask("si", [
+         "shell:killShare",
+         "shell:startShareInc"
       ]);
 
-      grunt.registerTask('restartShare', [
-         'shell:killShare',
-         'shell:startShare'
+      grunt.registerTask("restartShare", [
+         "shell:killShare",
+         "shell:startShare"
       ]);
 
       // Svn up shorthand.
-      grunt.registerTask('up', [
-         'shell:svnUp'
+      grunt.registerTask("up", [
+         "shell:svnUp"
       ]);
 
       // Build & start after a Clean & UPdate
-      grunt.registerTask('cup', [
-         'shell:killRepo',
-         'shell:killShare',
-         'shell:mvnClean',
-         'shell:svnUp',
-         'shell:npmInstall',
-         'shell:startRepoFull',
-         'shell:startShareInc'
+      grunt.registerTask("cup", [
+         "shell:killRepo",
+         "shell:killShare",
+         "shell:mvnClean",
+         "shell:svnUp",
+         "shell:npmInstall",
+         "shell:startRepoFull",
+         "shell:startShareInc"
       ]);
 
-      grunt.registerTask('sel', [
-         'shell:seleniumUp'
+      grunt.registerTask("sel", [
+         "shell:seleniumUp"
       ]);
 
-      grunt.registerTask('down', [
-         'shell:killRepo',
-         'shell:killShare',
-         'vdown'
+      grunt.registerTask("down", [
+         "shell:killRepo",
+         "shell:killShare",
+         "vdown"
       ]);
 
-      grunt.registerTask('hideExistingCoverageReports', [
-         'copy:coverageReportsToTemp',
-         'clean:coverageReports'
+      grunt.registerTask("hideExistingCoverageReports", [
+         "copy:coverageReportsToTemp",
+         "clean:coverageReports"
       ]);
 
-      grunt.registerTask('showExistingCoverageReports', [
-         'copy:coverageReportsFromTemp',
-         'clean:coverageReportsTemp'
+      grunt.registerTask("showExistingCoverageReports", [
+         "copy:coverageReportsFromTemp",
+         "clean:coverageReportsTemp"
       ]);
 
-      grunt.registerTask('scb', [
-         'shell:scb'
+      grunt.registerTask("scb", [
+         "shell:scb"
       ]);
 
       // Re-build test app for development purposes...
-      grunt.registerTask('utd', [
-         'shell:stopTestApp',
-         'shell:se',
-         'startUnitTestApp',
-         'waitServer'
+      grunt.registerTask("utd", [
+         "shell:stopTestApp",
+         "shell:se",
+         "startUnitTestApp",
+         "waitServer"
       ]);
 
       // Display notifications on test passes and failures...
-      var notify = require('./node_modules/grunt-notify/lib/notify-lib');
+      var notify = require("./node_modules/grunt-notify/lib/notify-lib");
       grunt.event.on("intern.fail", function(data) {
          notify({
             title: "Unit Test Failed",
