4710f55d502738646a6df01c2cee3015c02430f9
Martin Doyle <martin.doyle@alfresco.com>
2015-03-12 09:34:00 +0000

    AKU-33: Add module documentation and decorate widgets with an attachpoint-name class, in order to make it more testable

--- aikau/src/main/resources/alfresco/documentlibrary/views/AlfDetailedViewItem.js
+++ aikau/src/main/resources/alfresco/documentlibrary/views/AlfDetailedViewItem.js
@@ -18,6 +18,10 @@
  */
 
 /**
+ * This module represents a single item to be rendered by the [AlfDetailedView]{@link module:alfresco/documentlibrary/views/AlfDetailedView}
+ * module. For performance reasons, it has been hard-coded into a specific template and with custom widget information, to avoid needing to use
+ * multiple layout widgets to achieve the same effect. For that reason, this module is unsuitable for extending.
+ *
  * @module alfresco/documentlibrary/views/AlfDetailedViewItem
  * @extends module:alfresco/lists/views/layouts/Row
  * @author Martin Doyle
@@ -31,205 +35,210 @@ define(["alfresco/lists/views/layouts/Row",
         "dojo/text!./templates/AlfDetailedViewItem.html"], 
         function(Row, Selector, array, lang, declare, domClass, template) {
 
-   return declare([Row], {
+      return declare([Row], {
 
-      /**
-       * An array of the CSS files to use with this widget.
-       *
-       * @instance
-       * @type {object[]}
-       * @default [{cssFile:"./css/AlfDetailedViewItem.css"}]
-       */
-      cssRequirements: [{
-         cssFile: "./css/AlfDetailedViewItem.css"
-      }],
+         /**
+          * An array of the CSS files to use with this widget.
+          *
+          * @instance
+          * @type {object[]}
+          * @default [{cssFile:"./css/AlfDetailedViewItem.css"}]
+          */
+         cssRequirements: [{
+            cssFile: "./css/AlfDetailedViewItem.css"
+         }],
 
-      /**
-       * The HTML template to use for the widget.
-       *
-       * @instance
-       * @type {String}
-       */
-      templateString: template,
+         /**
+          * The HTML template to use for the widget.
+          *
+          * @instance
+          * @type {String}
+          */
+         templateString: template,
 
-      /**
-       * Function run after widget created.
-       *
-       * @instance
-       */
-      postCreate: function alfresco_documentlibrary_views_AlfDetailedViewItem__postCreate() {
+         /**
+          * Function run after widget created.
+          *
+          * @instance
+          */
+         postCreate: function alfresco_documentlibrary_views_AlfDetailedViewItem__postCreate() {
 
-         // Don't break the chain
-         this.inherited(arguments);
+            // Don't break the chain
+            this.inherited(arguments);
 
-         // Handle conditional elements
-         if (this.currentItem.workingCopy && this.currentItem.workingCopy.isWorkingCopy) {
-            domClass.add(this.domNode, "alfresco-documentlibrary-views-AlfDetailedViewItem--working-copy");
-         }
-
-         // Build the widgets
-         array.forEach(this.widgetsAndAttachPoints, function(widgetConfig) {
-
-            // Create an instance of the config (so we can modify it)
-            var configInstance = lang.clone(widgetConfig),
-               attachPoint = this[configInstance._attachPoint],
-               renderWidget = !configInstance._render || configInstance._render.call(this, this.currentItem);
-
-            // Make sure we should render this widget
-            if (renderWidget) {
-
-               // Remove the "custom" properties on the widget config
-               delete configInstance._attachPoint;
-               delete configInstance._render;
-
-               // Create the widget
-               this.createWidget(configInstance, attachPoint);
-
-            } else {
-
-               // Remove the unused dom node
-               attachPoint.parentNode.removeChild(attachPoint);
+            // Handle conditional elements
+            if (this.currentItem.workingCopy && this.currentItem.workingCopy.isWorkingCopy) {
+               domClass.add(this.domNode, "alfresco-documentlibrary-views-AlfDetailedViewItem--working-copy");
             }
-         }, this);
-      },
 
-      /**
-       * A map of widget configs to be applied to attach points, where the key is the attach point and the value is the configuration for the widget.
-       *
-       * @protected
-       * @type {Object} The attachpoint to widget-configurations map
-       */
-      widgetsAndAttachPoints: [{
-         _attachPoint: "selector",
-         name: "alfresco/renderers/Selector",
-         config: {
-            publishGlobal: false,
-            publishToParent: true
-         }
-      }, {
-         _attachPoint: "indicators",
-         name: "alfresco/renderers/Indicators"
-      }, {
-         _attachPoint: "thumbnail",
-         name: "alfresco/renderers/Thumbnail",
-         config: {
-            showDocumentPreview: true,
-            publishGlobal: false,
-            publishToParent: true
-         }
-      }, {
-         _attachPoint: "lockedBanner",
-         name: "alfresco/renderers/LockedBanner"
-      }, {
-         _attachPoint: "name",
-         name: "alfresco/renderers/InlineEditPropertyLink",
-         config: {
-            propertyToRender: "node.properties.cm:name",
-            postParam: "prop_cm_name",
-            renderSize: "large"
-         }
-      }, {
-         _attachPoint: "title",
-         _render: function(item) {
-            return item.node.properties["cm:title"];
+            // Build the widgets
+            array.forEach(this.widgetsAndAttachPoints, function(widgetConfig) {
+
+               // Create an instance of the config (so we can modify it)
+               var configInstance = lang.clone(widgetConfig),
+                  attachPointName = configInstance._attachPoint,
+                  attachPoint = this[attachPointName],
+                  renderWidget = !configInstance._render || configInstance._render.call(this, this.currentItem),
+                  newWidget;
+
+               // Make sure we should render this widget
+               if (renderWidget) {
+
+                  // Remove the "custom" properties on the widget config
+                  delete configInstance._attachPoint;
+                  delete configInstance._render;
+
+                  // Create the widget
+                  newWidget = this.createWidget(configInstance, attachPoint);
+
+                  // Add an appropriate class to the widget
+                  domClass.add(newWidget.domNode, "detail-item__" + attachPointName);
+
+               } else {
+
+                  // Remove the unused dom node
+                  attachPoint.parentNode.removeChild(attachPoint);
+               }
+            }, this);
          },
-         name: "alfresco/renderers/InlineEditProperty",
-         config: {
-            propertyToRender: "node.properties.cm:title",
-            postParam: "prop_cm_title",
-            renderedValuePrefix: "(",
-            renderedValueSuffix: ")"
-         }
-      }, {
-         _attachPoint: "version",
-         _render: function(item) {
-            return !item.node.isContainer && !(item.workingCopy && item.workingCopy.isWorkingCopy);
-         },
-         name: "alfresco/renderers/Version",
-         config: {
-            onlyShowOnHover: true
-         }
-      }, {
-         _attachPoint: "date",
-         name: "alfresco/renderers/Date"
-      }, {
-         _attachPoint: "size",
-         _render: function(item) {
-            return !item.node.isContainer;
-         },
-         name: "alfresco/renderers/Size"
-      }, {
-         _attachPoint: "description",
-         name: "alfresco/renderers/InlineEditProperty",
-         config: {
-            propertyToRender: "node.properties.cm:description",
-            postParam: "prop_cm_description",
-            warnIfNotAvailable: true,
-            warnIfNoteAvailableMessage: "no.description.message"
-         }
-      }, {
-         _attachPoint: "tags",
-         _render: function(item) {
-            return !(item.workingCopy && item.workingCopy.isWorkingCopy);
-         },
-         name: "alfresco/renderers/Tags",
-         config: {
-            propertyToRender: "node.properties.cm:taggable",
-            postParam: "prop_cm_taggable",
-            warnIfNotAvailable: true,
-            warnIfNoteAvailableMessage: "no.tags.message"
-         }
-      }, {
-         _attachPoint: "category",
-         _render: function(item) {
-            return item.node.properties["cm:categories"];
-         },
-         name: "alfresco/renderers/Category"
-      }, {
-         _attachPoint: "favourite",
-         _render: function(item) {
-            return !(item.workingCopy && item.workingCopy.isWorkingCopy);
-         },
-         name: "alfresco/renderers/Favourite"
-      }, {
-         _attachPoint: "like",
-         _render: function(item) {
-            return !(item.workingCopy && item.workingCopy.isWorkingCopy);
-         },
-         name: "alfresco/renderers/Like"
-      }, {
-         _attachPoint: "comments",
-         _render: function(item) {
-            return !(item.workingCopy && item.workingCopy.isWorkingCopy);
-         },
-         name: "alfresco/renderers/Comments",
-         config: {
-            subscriptionTopic: "ALF_COMMENTS_LOADED",
-            publishTopic: "ALF_REVEAL_COMMENTS",
-            publishPayload: {}
-         }
-      }, {
-         _attachPoint: "quickShare",
-         _render: function(item) {
-            return !item.node.isContainer && !(item.workingCopy && item.workingCopy.isWorkingCopy);
-         },
-         name: "alfresco/renderers/QuickShare"
-      }, {
-         _attachPoint: "commentsReveal",
-         name: "alfresco/layout/VerticalReveal",
-         config: {
-            subscriptionTopic: "ALF_REVEAL_COMMENTS",
-            widgets: [{
-               name: "alfresco/renderers/CommentsList"
-            }]
-         }
-      }, {
-         _attachPoint: "actions",
-         name: "alfresco/renderers/Actions",
-         config: {
-            publishGlobal: false,
-            publishToParent: true
-         }
-      }]
+
+         /**
+          * A map of widget configs to be applied to attach points, where the key is the attach point and the value is the configuration for the widget.
+          *
+          * @protected
+          * @type {Object}
+          */
+         widgetsAndAttachPoints: [{
+            _attachPoint: "selector",
+            name: "alfresco/renderers/Selector",
+            config: {
+               publishGlobal: false,
+               publishToParent: true
+            }
+         }, {
+            _attachPoint: "indicators",
+            name: "alfresco/renderers/Indicators"
+         }, {
+            _attachPoint: "thumbnail",
+            name: "alfresco/renderers/Thumbnail",
+            config: {
+               showDocumentPreview: true,
+               publishGlobal: false,
+               publishToParent: true
+            }
+         }, {
+            _attachPoint: "lockedBanner",
+            name: "alfresco/renderers/LockedBanner"
+         }, {
+            _attachPoint: "name",
+            name: "alfresco/renderers/InlineEditPropertyLink",
+            config: {
+               propertyToRender: "node.properties.cm:name",
+               postParam: "prop_cm_name",
+               renderSize: "large"
+            }
+         }, {
+            _attachPoint: "title",
+            _render: function(item) {
+               return item.node.properties["cm:title"];
+            },
+            name: "alfresco/renderers/InlineEditProperty",
+            config: {
+               propertyToRender: "node.properties.cm:title",
+               postParam: "prop_cm_title",
+               renderedValuePrefix: "(",
+               renderedValueSuffix: ")"
+            }
+         }, {
+            _attachPoint: "version",
+            _render: function(item) {
+               return !item.node.isContainer && !(item.workingCopy && item.workingCopy.isWorkingCopy);
+            },
+            name: "alfresco/renderers/Version",
+            config: {
+               onlyShowOnHover: true
+            }
+         }, {
+            _attachPoint: "date",
+            name: "alfresco/renderers/Date"
+         }, {
+            _attachPoint: "size",
+            _render: function(item) {
+               return !item.node.isContainer;
+            },
+            name: "alfresco/renderers/Size"
+         }, {
+            _attachPoint: "description",
+            name: "alfresco/renderers/InlineEditProperty",
+            config: {
+               propertyToRender: "node.properties.cm:description",
+               postParam: "prop_cm_description",
+               warnIfNotAvailable: true,
+               warnIfNoteAvailableMessage: "no.description.message"
+            }
+         }, {
+            _attachPoint: "tags",
+            _render: function(item) {
+               return !(item.workingCopy && item.workingCopy.isWorkingCopy);
+            },
+            name: "alfresco/renderers/Tags",
+            config: {
+               propertyToRender: "node.properties.cm:taggable",
+               postParam: "prop_cm_taggable",
+               warnIfNotAvailable: true,
+               warnIfNoteAvailableMessage: "no.tags.message"
+            }
+         }, {
+            _attachPoint: "category",
+            _render: function(item) {
+               return item.node.properties["cm:categories"];
+            },
+            name: "alfresco/renderers/Category"
+         }, {
+            _attachPoint: "favourite",
+            _render: function(item) {
+               return !(item.workingCopy && item.workingCopy.isWorkingCopy);
+            },
+            name: "alfresco/renderers/Favourite"
+         }, {
+            _attachPoint: "like",
+            _render: function(item) {
+               return !(item.workingCopy && item.workingCopy.isWorkingCopy);
+            },
+            name: "alfresco/renderers/Like"
+         }, {
+            _attachPoint: "comments",
+            _render: function(item) {
+               return !(item.workingCopy && item.workingCopy.isWorkingCopy);
+            },
+            name: "alfresco/renderers/Comments",
+            config: {
+               subscriptionTopic: "ALF_COMMENTS_LOADED",
+               publishTopic: "ALF_REVEAL_COMMENTS",
+               publishPayload: {}
+            }
+         }, {
+            _attachPoint: "quickShare",
+            _render: function(item) {
+               return !item.node.isContainer && !(item.workingCopy && item.workingCopy.isWorkingCopy);
+            },
+            name: "alfresco/renderers/QuickShare"
+         }, {
+            _attachPoint: "commentsReveal",
+            name: "alfresco/layout/VerticalReveal",
+            config: {
+               subscriptionTopic: "ALF_REVEAL_COMMENTS",
+               widgets: [{
+                  name: "alfresco/renderers/CommentsList"
+               }]
+            }
+         }, {
+            _attachPoint: "actions",
+            name: "alfresco/renderers/Actions",
+            config: {
+               publishGlobal: false,
+               publishToParent: true
+            }
+         }]
+      });
    });
-});
