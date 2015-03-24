a8a588d8e32cf820d606b97bf47f70d3f2536039
Dave Draper <david.draper@alfresco.com>
2015-03-24 12:23:35 +0000

    AKU-37: Test updates

--- aikau/src/test/resources/alfresco/documentlibrary/ViewPreferencesGroupTest.js
+++ aikau/src/test/resources/alfresco/documentlibrary/ViewPreferencesGroupTest.js
@@ -38,6 +38,122 @@ define(["intern!object",
 
       beforeEach: function() {
          browser.end();
+      },
+
+      "Set an initial node": function() {
+         // Click the first button to set a node (that has a default view that is the current view)...
+         // Remove should be shown with correct label
+         // Add should not be shown (because it's the current default)
+         return browser.findByCssSelector("#SET_METADATA_1_label")
+            .click()
+         .end()
+         .findByCssSelector("#DROP_DOWN_MENU_text")
+            .click()
+         .end()
+         .findByCssSelector("#REMOVE_DEFAULT_DOCUMENT_LIST_VIEW_text")
+            .isDisplayed()
+            .then(function(displayed) {
+               assert.isTrue(displayed, "The remove menu item was not displayed");
+            });
+      },
+
+      "Check remove label is correct": function() {
+         return browser.findByCssSelector("#REMOVE_DEFAULT_DOCUMENT_LIST_VIEW_text")
+            .getVisibleText()
+            .then(function(text) {
+               assert.equal(text, "Remove \"View 1\" as the default view for this folder", "The remove label was not updated correctly");
+            });
+      },
+
+      "Check that the add menu item is hidden": function() {
+         return browser.findByCssSelector("#SET_DEFAULT_DOCUMENT_LIST_VIEW_text")
+            .isDisplayed()
+            .then(function(displayed) {
+               assert.isFalse(displayed, "The add menu item was displayed unexpectedly");
+            });
+      },
+
+      "Change the view": function() {
+         // Remove should still be shown
+         // Add should also be shown (because the current view is not the default view)
+         return browser.findByCssSelector("#SET_VIEW_2_text")
+            .click()
+         .end()
+         .findByCssSelector("#DROP_DOWN_MENU_text")
+            .click()
+         .end()
+         .findByCssSelector("#SET_DEFAULT_DOCUMENT_LIST_VIEW_text")
+            .isDisplayed()
+            .then(function(displayed) {
+               assert.isTrue(displayed, "The set menu item was not displayed");
+            });
+      },
+
+      "Check that the remove menu item is still shown": function() {
+          return browser.findByCssSelector("#REMOVE_DEFAULT_DOCUMENT_LIST_VIEW_text")
+            .isDisplayed()
+            .then(function(displayed) {
+               assert.isTrue(displayed, "The remove menu item was not displayed");
+            });
+      },
+
+      "Change the view back": function() {
+         // Remove should still be shown
+         // Add should not be shown (because the new node has a default view)
+         return browser.findByCssSelector("#SET_VIEW_1_text")
+            .click()
+         .end()
+         .findByCssSelector("#DROP_DOWN_MENU_text")
+            .click()
+         .end()
+         .findByCssSelector("#SET_DEFAULT_DOCUMENT_LIST_VIEW_text")
+            .isDisplayed()
+            .then(function(displayed) {
+               assert.isFalse(displayed, "The set menu item was displayed unexpectedly");
+            });
+      },
+
+      "Change the node to check view is changed": function() {
+         return browser.findByCssSelector("#SET_METADATA_2_label")
+            .click()
+         .end()
+         .findAllByCssSelector(TestCommon.topicSelector("ALF_DOCLIST_SELECT_VIEW", "publish", "last"))
+            .then(function(elements) {
+               assert.lengthOf(elements, 1, "View selection publication for default folder view not found");
+            })
+         .end()
+         .findByCssSelector(TestCommon.pubSubDataValueCssSelector("last", "value"))
+            .getVisibleText()
+            .then(function(text) {
+               assert.equal(text, "VIEW2", "The default folder view was not requested");
+            });
+      },
+
+      "Change to unowned node with default view": function() {
+         // The menu items should both be hidden
+         // Default view should be requested
+         return browser.findByCssSelector("#SET_METADATA_3_label")
+            .click()
+         .end()
+         .findAllByCssSelector(TestCommon.topicSelector("ALF_DOCLIST_SELECT_VIEW", "publish", "last"))
+            .then(function(elements) {
+               assert.lengthOf(elements, 1, "View selection publication for default folder view not found");
+            })
+         .end()
+         .findByCssSelector(TestCommon.pubSubDataValueCssSelector("last", "value"))
+            .getVisibleText()
+            .then(function(text) {
+               assert.equal(text, "VIEW1", "The default folder view was not requested");
+            });
+      },
+
+      "Change to unowned node without default view": function() {
+         // The menu items should still be hidden
+         // The user preferred view should be requested
+      },
+
+      "Post Coverage Results": function() {
+         TestCommon.alfPostCoverageResults(this, browser);
       }
    });
 });
