9a05e152c405d355e0d09400761237d13cd26605
Martin Doyle <martin.doyle@alfresco.com>
2015-04-07 10:14:42 +0100

    AKU-178: Multi select input - Commit work-in-progress

--- aikau/src/main/resources/alfresco/forms/controls/MultiSelect.js
+++ aikau/src/main/resources/alfresco/forms/controls/MultiSelect.js
@@ -28,13 +28,15 @@ define([
       "dijit/_WidgetBase",
       "dojo/_base/declare",
       "dojo/_base/lang",
+      "dojo/Deferred",
       "dojo/dom-construct",
       "dojo/dom-style",
       "dojo/dom-class",
+      "dojo/keys",
       "dojo/on",
       "dojo/text!./templates/MultiSelect.html"
    ],
-   function(_TemplatedMixin, _WidgetBase, declare, lang, domConstruct, domStyle, domClass, on, template) {
+   function(_TemplatedMixin, _WidgetBase, declare, lang, Deferred, domConstruct, domStyle, domClass, keys, on, template) {
 
       return declare([_WidgetBase, _TemplatedMixin], {
 
@@ -68,13 +70,60 @@ define([
          rootClass: "alfresco-forms-controls-MultiSelect",
 
          /**
-          * Constructor
+          * A cache of the current search value
+          *
+          * @type {string}
+          */
+         _currentSearchValue: "",
+
+         /**
+          * The index of the latest search request
+          *
+          * @type {number}
+          */
+         _latestSearchRequestIndex: 0,
+
+         /**
+          * Timeout used to debounce new search requests
+          *
+          * @type {number}
+          */
+         _newSearchTimeout: 0,
+
+         /**
+          * A cache of the last search value
+          *
+          * @type {string}
+          */
+         _prevSearchValue: null,
+
+         /**
+          * The number of milliseconds to debounce search requests, i.e. the pause
+          * needed during typing for a search request to actually kick off
+          *
+          * @type {number}
+          */
+         _searchDebounceMs: 250,
+
+         /**
+          * TEMPORARY - Override of proper functionality for development purposes
           *
           * @override
           * @instance
+          * @returns  {object} A dojo/promise/Promise object
           */
-         constructor: function() {
-            window.alfMs = this;
+         alfPublishToPromise: function() {
+            console.log("alfPublishToPromise...");
+            var deferred = new Deferred();
+            setTimeout(function() {
+               // deferred.resolve({
+               //    aloha: "Hi there"
+               // });
+               deferred.reject({
+                  message: "An expected error occurred - \"Wibble!\""
+               });
+            }, 1000);
+            return deferred.promise;
          },
 
          /**
@@ -83,9 +132,19 @@ define([
           * @override
           * @instance
           */
-         buildRendering: function() {
+         buildRendering: function alfresco_forms_controls_MultiSelect__buildRendering() {
             this.inherited(arguments);
             this.width && domStyle.set(this.domNode, "width", this.width);
+         },
+
+         /**
+          * Constructor
+          *
+          * @override
+          * @instance
+          */
+         constructor: function alfresco_forms_controls_MultiSelect__constructor() {
+            window.alfMs = this;
          },
 
          /**
@@ -94,8 +153,9 @@ define([
           * @override
           * @instance
           */
-         postCreate: function() {
+         postCreate: function alfresco_forms_controls_MultiSelect__postCreate() {
             this.inherited(arguments);
+            this.own(on(this.domNode, "click", lang.hitch(this, this._onControlClick)));
             this.own(on(this.searchBox, "focus", lang.hitch(this, this._onSearchFocus)));
             this.own(on(this.searchBox, "blur", lang.hitch(this, this._onSearchBlur)));
          },
@@ -106,7 +166,7 @@ define([
           * @override
           * @instance
           */
-         startup: function() {
+         startup: function alfresco_forms_controls_MultiSelect__startup() {
             this.inherited(arguments);
          },
 
@@ -116,27 +176,272 @@ define([
           * @instance
           * @param    {string[]} newValues The new values
           */
-         setValue: function(newValue) {
+         setValue: function alfresco_forms_controls_MultiSelect__setValue(newValue) {
             /*jshint unused:false*/
             // NOOP
          },
 
-         _onSearchBlur: function() {
+         /**
+          * Do not fire multiple searches needlessly. Debounce the search requests,
+          * i.e. wait until the user has paused typing to actually do the search.
+          *
+          * @instance
+          * @protected
+          * @param    {string} searchString The search string to use
+          */
+         _debounceNewSearch: function alfresco_forms_controls_MultiSelect___debounceNewSeach(searchString) {
+            clearTimeout(this._newSearchTimeout);
+            this._newSearchTimeout = setTimeout(lang.hitch(this, function() {
+               this._startSearch(searchString);
+            }), this._searchDebounceMs);
+         },
+
+         /**
+          * Handle failures that occur when calling the search service
+          *
+          * @instance
+          * @protected
+          * @param    {object} err The error object
+          */
+         _handleSearchFailure: function alfresco_forms_controls_MultiSelect___handleSearchFailure(err) {
+            this._hideLoading();
+            this._showError(err.message);
+            this.alfLog("error", "Error occurred during search: ", err);
+         },
+
+         /**
+          * Handle the (successful) response from the search service
+          *
+          * @instance
+          * @protected
+          * @param    {object} response The response
+          */
+         _handleSearchSuccess: function alfresco_forms_controls_MultiSelect___handleSearchSuccess(response) {
+            this._hideLoading();
+            console.warn("Response received from search service: ", response);
+         },
+
+         /**
+          * Hide the error dropdown
+          *
+          * @instance
+          * @protected
+          */
+         _hideError: function alfresco_forms_controls_MultiSelect___hideError() {
+            domClass.remove(this.domNode, this.rootClass + "--show-error");
+         },
+
+         /**
+          * Hide the loading dropdown
+          *
+          * @instance
+          * @protected
+          */
+         _hideLoading: function alfresco_forms_controls_MultiSelect___hideLoading() {
+            domClass.remove(this.domNode, this.rootClass + "--show-loading");
+
+         },
+
+         /**
+          * Hide the results dropdown
+          *
+          * @instance
+          * @protected
+          */
+         _hideResultsDropdown: function alfresco_forms_controls_MultiSelect___hideResults() {
+            domClass.remove(this.domNode, this.rootClass + "--show-results");
+            this._hideLoading();
+            this._hideError();
+         },
+
+         /**
+          * Handle clicks on the control
+          *
+          * @instance
+          * @protected
+          * @param    {object} evt Dojo-normalised event object
+          */
+         _onControlClick: function alfresco_forms_controls_MultiSelect___onControlClick(evt) {
+            var controlIsFocused = domClass.contains(this.domNode, this.rootClass + "--focused");
+            if (evt.target !== this.searchBox && !controlIsFocused) {
+               this.searchBox.focus();
+            }
+         },
+
+         /**
+          * Handle blur events on the search box
+          *
+          * @instance
+          * @protected
+          * @param {object} evt Dojo-normalised event object
+          */
+         _onSearchBlur: function alfresco_forms_controls_MultiSelect___onSearchBlur(evt) {
+            /*jshint unused:false*/
             domClass.remove(this.domNode, this.rootClass + "--focused");
+            this._hideResultsDropdown();
          },
 
-         _onSearchChange: function() {
-            this.offScreenSearch.innerHTML = this.searchBox.value;
+         /**
+          * Handle changes to the search box value
+          *
+          * @instance
+          * @protected
+          * @param {string} newValue The new search value
+          */
+         _onSearchChange: function alfresco_forms_controls_MultiSelect___onSearchChange(newValue) {
+
+            // Get new value and update stored value
+            this._prevSearchValue = this._currentSearchValue;
+            this._currentSearchValue = newValue;
+
+            // Update searchbox size
+            this.offScreenSearch.innerHTML = newValue;
             var contentWidth = this.offScreenSearch.offsetWidth;
-            domStyle.set(this.searchBox, "width", (contentWidth + 10) + "px");
+            domStyle.set(this.searchBox, "width", (contentWidth + 20) + "px");
+
+            // Start a new search
+            this._debounceNewSearch(newValue);
          },
 
-         _onSearchFocus: function() {
+         /**
+          * Handle focus events on this control
+          *
+          * @instance
+          * @protected
+          * @param {object} evt Dojo-normalised event object
+          */
+         _onSearchFocus: function alfresco_forms_controls_MultiSelect___onSearchFocus(evt) {
+            /*jshint unused:false*/
             domClass.add(this.domNode, this.rootClass + "--focused");
+            this._showResults();
          },
 
-         _onSearchKeyUp: function() {
-            this._onSearchChange();
+         /**
+          * Handle keypress events on the search box
+          *
+          * @instance
+          * @protected
+          * @param {object} evt Dojo-normalised event object
+          */
+         _onSearchKeypress: function alfresco_forms_controls_MultiSelect___onSearchKeypress(evt) {
+            switch (evt.charOrCode) {
+               case keys.ESCAPE:
+                  this._resetSearchBox();
+                  this._hideResultsDropdown();
+                  break;
+               case keys.DOWN_ARROW:
+                  this._showResults();
+                  break;
+               default:
+                  // Allow to continue
+            }
+         },
+
+         /**
+          * Handle keyup events on the search box
+          *
+          * @instance
+          * @protected
+          * @param {object} evt Dojo-normalised event object
+          */
+         _onSearchKeyup: function alfresco_forms_controls_MultiSelect___onSearchKeyup(evt) {
+            /*jshint unused:false*/
+            this._onSearchUpdate();
+         },
+
+         /**
+          * Handle updates to the search box, which may or may not result in
+          * the search value having changed
+          *
+          * @instance
+          * @protected
+          */
+         _onSearchUpdate: function alfresco_forms_controls_MultiSelect___onSearchUpdate() {
+            var trimmedValue = this.searchBox.value.replace(/^\s+|\s+$/g, "");
+            if (this._currentSearchValue !== trimmedValue) {
+               this._onSearchChange(trimmedValue);
+            }
+         },
+
+         /**
+          * Reset the search box (i.e. empty it)
+          *
+          * @instance
+          * @protected
+          */
+         _resetSearchBox: function alfresco_forms_controls_MultiSelect___resetSearchBox() {
+            this.searchBox.value = "";
+         },
+
+         /**
+          * Show the error dropdown
+          *
+          * @instance
+          * @protected
+          * @param {string} message The error message to be shown
+          */
+         _showError: function alfresco_forms_controls_MultiSelect___showError(message) {
+
+            // Remove old message and insert new one (safely!)
+            while (this.errorItem.hasChildNodes()) {
+               this.errorItem.removeChild(this.errorItem.firstChild);
+            }
+            this.errorItem.appendChild(document.createTextNode(message));
+
+            // Show the error (and hide any loading indicator)
+            domClass.add(this.domNode, this.rootClass + "--show-error");
+            this._hideLoading();
+            this._showResults();
+         },
+
+         /**
+          * Show the loading dropdown
+          *
+          * @instance
+          * @protected
+          */
+         _showLoading: function alfresco_forms_controls_MultiSelect___showLoading() {
+            domClass.add(this.domNode, this.rootClass + "--show-loading");
+            this._hideError();
+            this._showResults();
+         },
+
+         /**
+          * Show the results dropdown
+          *
+          * @instance
+          * @protected
+          */
+         _showResults: function alfresco_forms_controls_MultiSelect___showResults() {
+            domClass.add(this.domNode, this.rootClass + "--show-results");
+         },
+
+         /**
+          * Start a new search
+          *
+          * @instance
+          * @protected
+          * @param    {string} searchString The string to search on
+          */
+         _startSearch: function alfresco_forms_controls_MultiSelect___startSearch(searchString) {
+            /*jshint unused:false*/
+            var thisRequestIndex = this._latestSearchRequestIndex = this._latestSearchRequestIndex + 1,
+               successHandler = lang.hitch(this, function(response) {
+                  if (thisRequestIndex === this._latestSearchRequestIndex) {
+                     this._handleSearchSuccess(response);
+                  }
+               }),
+               failureHandler = lang.hitch(this, function(err) {
+                  if (thisRequestIndex === this._latestSearchRequestIndex) {
+                     this._handleSearchFailure(err);
+                  }
+               });
+            if (searchString) {
+               this._showLoading();
+               this.alfPublishToPromise().then(successHandler, failureHandler);
+            } else {
+               this._hideResultsDropdown();
+            }
          }
       });
    }
