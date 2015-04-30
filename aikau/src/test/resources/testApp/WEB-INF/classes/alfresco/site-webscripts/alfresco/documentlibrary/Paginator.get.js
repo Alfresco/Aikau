b62964ef62f538b7956df38abf7dda405b210925
Martin Doyle <martin.doyle@alfresco.com>
2015-04-30 15:32:47 +0100

    AKU-248: Selecting filter doesn't reset to first page - Update doclist and add tests to PaginationTest

--- aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/documentlibrary/Paginator.get.js
+++ aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/documentlibrary/Paginator.get.js
@@ -16,7 +16,8 @@ model.jsonModel = {
          config: {
             loadDataSubscriptionTopic: "ALF_RETRIEVE_DOCUMENTS_REQUEST"
          }
-      }
+      },
+      "alfresco/services/NavigationService"
    ],
    widgets: [
       {
@@ -96,52 +97,247 @@ model.jsonModel = {
                   }
                },
                {
-                  id: "CUSTOM_PAGE_SIZES",
-                  name: "alfresco/layout/ClassicWindow",
+                  name: "alfresco/layout/VerticalWidgets",
                   config: {
-                     title: "Custom page sizes",
-                     pubSubScope: "CUSTOM_",
                      widgets: [
                         {
-                           id: "MENU_BAR",
-                           name: "alfresco/menus/AlfMenuBar",
+                           name: "alfresco/layout/LeftAndRight",
                            config: {
                               widgets: [
                                  {
-                                    id: "CUSTOM_PAGE_SIZE_PAGINATOR",
-                                    name: "alfresco/lists/Paginator",
+                                    id: "CHANGE_FILTER",
+                                    name: "alfresco/buttons/AlfButton",
                                     config: {
-                                       documentsPerPage: 10,
-                                       hidePageSizeOnWidth: 100,
-                                       pageSizes: [5,10,20]
+                                       label: "Change Filter",
+                                       pubSubScope: "CUSTOM_",
+                                       publishTopic: "ALF_DOCLIST_FILTER_SELECTION",
+                                       publishPayload: {
+                                          value: "myfilter"
+                                       }
+                                    }
+                                 },
+                                 {
+                                    id: "CHANGE_PATH",
+                                    name: "alfresco/buttons/AlfButton",
+                                    config: {
+                                       label: "Change Path",
+                                       pubSubScope: "CUSTOM_",
+                                       publishTopic: "ALF_DOCUMENTLIST_PATH_CHANGED",
+                                       publishPayload: {
+                                          path: "mypath"
+                                       }
+                                    }
+                                 },
+                                 {
+                                    id: "CHANGE_TAG",
+                                    name: "alfresco/buttons/AlfButton",
+                                    config: {
+                                       label: "Change Tag",
+                                       pubSubScope: "CUSTOM_",
+                                       publishTopic: "ALF_DOCUMENTLIST_TAG_CHANGED",
+                                       publishPayload: {
+                                          tag: "mytag"
+                                       }
+                                    }
+                                 },
+                                 {
+                                    id: "CHANGE_CATEGORY",
+                                    name: "alfresco/buttons/AlfButton",
+                                    config: {
+                                       label: "Change Category",
+                                       pubSubScope: "CUSTOM_",
+                                       publishTopic: "ALF_DOCUMENTLIST_CATEGORY_CHANGED",
+                                       publishPayload: {
+                                          category: "mycategory"
+                                       }
                                     }
                                  }
                               ]
                            }
                         },
                         {
-                           id: "LIST",
-                           name: "alfresco/lists/AlfSortablePaginatedList",
+                           id: "CUSTOM_PAGE_SIZES",
+                           name: "alfresco/layout/ClassicWindow",
                            config: {
-                              useHash: false,
-                              currentPageSize: 10,
+                              title: "Custom page sizes",
+                              pubSubScope: "CUSTOM_",
                               widgets: [
                                  {
-                                    name: "alfresco/lists/views/AlfListView",
+                                    id: "MENU_BAR",
+                                    name: "alfresco/menus/AlfMenuBar",
                                     config: {
                                        widgets: [
                                           {
-                                             name: "alfresco/lists/views/layouts/Row",
+                                             id: "CUSTOM_PAGE_SIZE_PAGINATOR",
+                                             name: "alfresco/lists/Paginator",
+                                             config: {
+                                                useHash: false,
+                                                documentsPerPage: 10,
+                                                hidePageSizeOnWidth: 100,
+                                                pageSizes: [5,10,20]
+                                             }
+                                          }
+                                       ]
+                                    }
+                                 },
+                                 {
+                                    id: "LIST",
+                                    name: "alfresco/documentlibrary/AlfDocumentList",
+                                    config: {
+                                       useHash: false,
+                                       currentPageSize: 10,
+                                       hashVarsForUpdate: [
+                                          "path",
+                                          "filter",
+                                          "tag",
+                                          "category"
+                                       ],
+                                       widgets: [
+                                          {
+                                             name: "alfresco/lists/views/AlfListView",
                                              config: {
                                                 widgets: [
                                                    {
-                                                      name: "alfresco/lists/views/layouts/Cell",
+                                                      name: "alfresco/lists/views/layouts/Row",
                                                       config: {
                                                          widgets: [
                                                             {
-                                                               name: "alfresco/renderers/Property",
+                                                               name: "alfresco/lists/views/layouts/Cell",
                                                                config: {
-                                                                  propertyToRender: "index"
+                                                                  widgets: [
+                                                                     {
+                                                                        name: "alfresco/renderers/Property",
+                                                                        config: {
+                                                                           propertyToRender: "index"
+                                                                        }
+                                                                     }
+                                                                  ]
+                                                               }
+                                                            }
+                                                         ]
+                                                      }
+                                                   }
+                                                ]
+                                             }
+                                          }
+                                       ]
+                                    }
+                                 }
+                              ]
+                           }
+                        },
+                        {
+                           name: "alfresco/layout/LeftAndRight",
+                           config: {
+                              widgets: [
+                                 {
+                                    id: "HASH_CHANGE_FILTER",
+                                    name: "alfresco/buttons/AlfButton",
+                                    config: {
+                                       label: "Change Filter",
+                                       pubSubScope: "HASH_CUSTOM_",
+                                       publishTopic: "ALF_DOCLIST_FILTER_SELECTION",
+                                       publishPayload: {
+                                          value: "myfilter"
+                                       }
+                                    }
+                                 },
+                                 {
+                                    id: "HASH_CHANGE_PATH",
+                                    name: "alfresco/buttons/AlfButton",
+                                    config: {
+                                       label: "Change Path",
+                                       pubSubScope: "HASH_CUSTOM_",
+                                       publishTopic: "ALF_DOCUMENTLIST_PATH_CHANGED",
+                                       publishPayload: {
+                                          path: "mypath"
+                                       }
+                                    }
+                                 },
+                                 {
+                                    id: "HASH_CHANGE_TAG",
+                                    name: "alfresco/buttons/AlfButton",
+                                    config: {
+                                       label: "Change Tag",
+                                       pubSubScope: "HASH_CUSTOM_",
+                                       publishTopic: "ALF_DOCUMENTLIST_TAG_CHANGED",
+                                       publishPayload: {
+                                          value: "mytag"
+                                       }
+                                    }
+                                 },
+                                 {
+                                    id: "HASH_CHANGE_CATEGORY",
+                                    name: "alfresco/buttons/AlfButton",
+                                    config: {
+                                       label: "Change Category",
+                                       pubSubScope: "HASH_CUSTOM_",
+                                       publishTopic: "ALF_DOCUMENTLIST_CATEGORY_CHANGED",
+                                       publishPayload: {
+                                          path: "mycategory"
+                                       }
+                                    }
+                                 }
+                              ]
+                           }
+                        },
+                        {
+                           id: "HASH_CUSTOM_PAGE_SIZES",
+                           name: "alfresco/layout/ClassicWindow",
+                           config: {
+                              title: "Custom page sizes",
+                              pubSubScope: "HASH_CUSTOM_",
+                              widgets: [
+                                 {
+                                    id: "HASH_MENU_BAR",
+                                    name: "alfresco/menus/AlfMenuBar",
+                                    config: {
+                                       widgets: [
+                                          {
+                                             id: "HASH_CUSTOM_PAGE_SIZE_PAGINATOR",
+                                             name: "alfresco/lists/Paginator",
+                                             config: {
+                                                useHash: false,
+                                                documentsPerPage: 10,
+                                                hidePageSizeOnWidth: 100,
+                                                pageSizes: [5,10,20]
+                                             }
+                                          }
+                                       ]
+                                    }
+                                 },
+                                 {
+                                    id: "HASH_LIST",
+                                    name: "alfresco/documentlibrary/AlfDocumentList",
+                                    config: {
+                                       useHash: true,
+                                       currentPageSize: 10,
+                                       hashVarsForUpdate: [
+                                          "path",
+                                          "filter",
+                                          "tag",
+                                          "category"
+                                       ],
+                                       widgets: [
+                                          {
+                                             name: "alfresco/lists/views/AlfListView",
+                                             config: {
+                                                widgets: [
+                                                   {
+                                                      name: "alfresco/lists/views/layouts/Row",
+                                                      config: {
+                                                         widgets: [
+                                                            {
+                                                               name: "alfresco/lists/views/layouts/Cell",
+                                                               config: {
+                                                                  widgets: [
+                                                                     {
+                                                                        name: "alfresco/renderers/Property",
+                                                                        config: {
+                                                                           propertyToRender: "index"
+                                                                        }
+                                                                     }
+                                                                  ]
                                                                }
                                                             }
                                                          ]
@@ -156,10 +352,9 @@ model.jsonModel = {
                               ]
                            }
                         }
-                     ]
-                  }
+                  ]
                }
-            ]
+            }]
          }
       },
       {
