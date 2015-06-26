a68b8891ee6eb05503af6b2919f3ba2a4d69f6d8
Martin Doyle <martin.doyle@alfresco.com>
2015-06-26 14:26:29 +0100

    AKU-337: Remove Dashlet Share dependencies - Update dashlet to include storage/retrieval of height as necessary

--- aikau/src/main/resources/alfresco/dashlets/Dashlet.js
+++ aikau/src/main/resources/alfresco/dashlets/Dashlet.js
@@ -27,289 +27,308 @@
  * @mixes external:dojo/_TemplatedMixin
  * @mixes module:alfresco/core/Core
  * @mixes module:alfresco/core/CoreWidgetProcessing*
- * @abstract
+ * @mixes module:alfresco/services/_DashletServiceTopicMixin
  * @author Erik Winlöf
+ * @author Martin Doyle
  */
 define(["dojo/_base/declare",
-        "dijit/_WidgetBase",
-        "dijit/_TemplatedMixin", 
-        "dojo/text!./templates/Dashlet.html", 
-        "alfresco/core/Core", 
-        "alfresco/core/CoreWidgetProcessing", 
-        "alfresco/core/ResizeMixin", 
-        "dojo/_base/lang", 
-        "dojo/_base/array", 
-        "dojo/dom-construct", 
-        "dojo/dom-class", 
-        "dojo/dom-style", 
-        "jquery", 
-        "jqueryui"], 
-        function(declare, _WidgetBase, _TemplatedMixin, template, AlfCore, CoreWidgetProcessing, ResizeMixin, lang, array, domConstruct, domClass, domStyle, $) {
+      "dijit/_WidgetBase",
+      "dijit/_TemplatedMixin",
+      "dojo/text!./templates/Dashlet.html",
+      "alfresco/core/Core",
+      "alfresco/core/CoreWidgetProcessing",
+      "alfresco/core/ResizeMixin",
+      "alfresco/services/_DashletServiceTopicMixin",
+      "dojo/_base/lang",
+      "dojo/_base/array",
+      "dojo/dom-construct",
+      "dojo/dom-class",
+      "dojo/dom-style",
+      "jquery",
+      "jqueryui"
+   ],
+   function(declare, _WidgetBase, _TemplatedMixin, template, AlfCore, CoreWidgetProcessing,
+      ResizeMixin, _DashletServiceTopicMixin, lang, array, domConstruct, domClass, domStyle, $) {
 
-   return declare([_WidgetBase, _TemplatedMixin, AlfCore, CoreWidgetProcessing, ResizeMixin], {
+      return declare([_WidgetBase, _TemplatedMixin, AlfCore, CoreWidgetProcessing, ResizeMixin, _DashletServiceTopicMixin], {
 
-      /**
-       * Id that will be used to store properties for this dashlet
-       * i.e. the dashlet height when using the resizer.
-       *
-       * @instance
-       * @type {string}
-       */
-      componentId: null,
+         /**
+          * Id that will be used to store properties for this dashlet
+          * i.e. the dashlet height when using the resizer.
+          *
+          * @instance
+          * @type {string}
+          */
+         componentId: null,
 
-      /**
-       * The base css class to use for this widget
-       *
-       * @instance
-       * @type {string}
-       * @default "alfresco-dashlets-Dashlet"
-       */
-      baseClass: "alfresco-dashlets-Dashlet",
+         /**
+          * The base css class to use for this widget
+          *
+          * @instance
+          * @type {string}
+          * @default "alfresco-dashlets-Dashlet"
+          */
+         baseClass: "alfresco-dashlets-Dashlet",
 
-      /**
-       * A second css class for sub classes of this widget to use to be able to target
-       * specifics in such dashlets.
-       *
-       * @instance
-       * @type {string}
-       * @default ""
-       */
-      subClass: "",
+         /**
+          * A second css class for sub classes of this widget to use to be able to target
+          * specifics in such dashlets.
+          *
+          * @instance
+          * @type {string}
+          * @default ""
+          */
+         subClass: "",
 
-      /**
-       * The i18n scope to use for this widget
-       *
-       * @instance
-       * @type {string}
-       */
-      i18nScope: "alfresco.dashlets.Dashlet",
+         /**
+          * The i18n scope to use for this widget
+          *
+          * @instance
+          * @type {string}
+          */
+         i18nScope: "alfresco.dashlets.Dashlet",
 
-      /**
-       * An array of the i18n files to use with this widget.
-       *
-       * @instance
-       * @type {object[]}
-       * @default [{i18nFile: "./i18n/Dashlet.properties"}]
-       */
-      i18nRequirements: [{i18nFile: "./i18n/Dashlet.properties"}],
+         /**
+          * An array of the i18n files to use with this widget.
+          *
+          * @instance
+          * @type {object[]}
+          * @default [{i18nFile: "./i18n/Dashlet.properties"}]
+          */
+         i18nRequirements: [{
+            i18nFile: "./i18n/Dashlet.properties"
+         }],
 
-      /**
-       * An array of the CSS files to use with this widget.
-       *
-       * @instance cssRequirements {Array}
-       * @type {object[]}
-       * @default [{cssFile:"./css/Dashlet.css"}]
-       */
-      cssRequirements: [{cssFile: "./css/Dashlet.css"}],
+         /**
+          * An array of the CSS files to use with this widget.
+          *
+          * @instance cssRequirements {Array}
+          * @type {object[]}
+          * @default [{cssFile:"./css/Dashlet.css"}]
+          */
+         cssRequirements: [{
+            cssFile: "./css/Dashlet.css"
+         }],
 
-      /**
-       * The HTML template to use for the widget.
-       * @instance
-       * @type {String}
-       */
-      templateString: template,
+         /**
+          * The HTML template to use for the widget.
+          * @instance
+          * @type {String}
+          */
+         templateString: template,
 
-      /**
-       * Config that makes it possible to dynamically update the title.
-       * Should be supplied in the format of
-       * {
-       *    titleConfig: {
-       *       topic: "SOME_TOPIC",
-       *       attribute: "path.to.topic.payload.attribute"
-       *    }
-       * }
-       *
-       * If there is no attribute present the entire payload value will be used as the title.
-       *
-       * @instance
-       * @type {object}
-       */
-      titleConfig: null,
+         /**
+          * Config that makes it possible to dynamically update the title.
+          * Should be supplied in the format of
+          * {
+          *    titleConfig: {
+          *       topic: "SOME_TOPIC",
+          *       attribute: "path.to.topic.payload.attribute"
+          *    }
+          * }
+          *
+          * If there is no attribute present the entire payload value will be used as the title.
+          *
+          * @instance
+          * @type {object}
+          */
+         titleConfig: null,
 
-      /**
-       * The container that holds the title.
-       * Will be populated by dojo.
-       *
-       * @instance
-       * @type {HTMLElement}
-       */
-      titleNode: null,
+         /**
+          * The container that holds the title.
+          * Will be populated by dojo.
+          *
+          * @instance
+          * @type {HTMLElement}
+          */
+         titleNode: null,
 
-      /**
-       * Widgets to place as title bar actions.
-       *
-       * @instance
-       * @type {object[]}
-       */
-      widgetsForTitleBarActions: null,
+         /**
+          * Widgets to place as title bar actions.
+          *
+          * @instance
+          * @type {object[]}
+          */
+         widgetsForTitleBarActions: null,
 
-      /**
-       * Widgets to place in the first toolbar
-       *
-       * @instance
-       * @type {object[]}
-       */
-      widgetsForToolbar: null,
+         /**
+          * Widgets to place in the first toolbar
+          *
+          * @instance
+          * @type {object[]}
+          */
+         widgetsForToolbar: null,
 
-      /**
-       * Widgets to place in the second toolbar
-       *
-       * @instance
-       * @type {object[]}
-       */
-      widgetsForToolbar2: null,
+         /**
+          * Widgets to place in the second toolbar
+          *
+          * @instance
+          * @type {object[]}
+          */
+         widgetsForToolbar2: null,
 
-      /**
-       * Widgets to place in the body
-       *
-       * @instance
-       * @type {object[]}
-       */
-      widgetsForBody: null,
+         /**
+          * Widgets to place in the body
+          *
+          * @instance
+          * @type {object[]}
+          */
+         widgetsForBody: null,
 
-      /**
-       * The container that holds the title bar action widgets.
-       * Will be populated by dojo.
-       *
-       * @instance
-       * @type {HTMLElement}
-       */
-      titleBarActionsNode: null,
+         /**
+          * The container that holds the title bar action widgets.
+          * Will be populated by dojo.
+          *
+          * @instance
+          * @type {HTMLElement}
+          */
+         titleBarActionsNode: null,
 
-      /**
-       * The container that holds the first toolbar widgets.
-       * Will be populated by dojo.
-       *
-       * @instance
-       * @type {HTMLElement}
-       */
-      toolbarNode: null,
+         /**
+          * The container that holds the first toolbar widgets.
+          * Will be populated by dojo.
+          *
+          * @instance
+          * @type {HTMLElement}
+          */
+         toolbarNode: null,
 
-      /**
-       * The container that holds the title second tollbar widgets.
-       * Will be populated by dojo.
-       *
-       * @instance
-       * @type {HTMLElement}
-       */
-      toolbar2Node: null,
+         /**
+          * The container that holds the title second tollbar widgets.
+          * Will be populated by dojo.
+          *
+          * @instance
+          * @type {HTMLElement}
+          */
+         toolbar2Node: null,
 
-      /**
-       * The container that holds the body widgets.
-       * Will be populated by dojo.
-       *
-       * @instance
-       * @type {HTMLElement}
-       */
-      bodyNode: null,
+         /**
+          * The container that holds the body widgets.
+          * Will be populated by dojo.
+          *
+          * @instance
+          * @type {HTMLElement}
+          */
+         bodyNode: null,
 
-      /**
-       * Explicit height in pixels of the dashlet body
-       *
-       * @instance
-       * @type {number}
-       */
-      bodyHeight: null,
+         /**
+          * Explicit height in pixels of the dashlet body
+          *
+          * @instance
+          * @type {number}
+          * @default
+          */
+         bodyHeight: null,
 
-      /**
-       * Whether resizing of this dashlet is disabled
-       *
-       * @type {boolean}
-       * @default
-       */
-      resizeDisabled: false,
+         /**
+          * Whether resizing of this dashlet is disabled
+          *
+          * @type {boolean}
+          * @default
+          */
+         resizeDisabled: false,
 
-      /**
-       * Implements the widget life-cycle method to add drag-and-drop upload capabilities to the root DOM node.
-       * This allows files to be dragged and dropped from the operating system directly into the browser
-       * and uploaded to the location represented by the document list.
-       *
-       * @instance
-       */
-      postCreate: function alfresco_dashlets_Dashlet__postCreate() {
-         this.processContainer(this.widgetsForTitleBarActions, this.titleBarActionsNode);
-         this.processContainer(this.widgetsForToolbar, this.toolbarNode);
-         this.processContainer(this.widgetsForToolbar2, this.toolbar2Node);
-         if (this.bodyHeight) {
-            domStyle.set(this.bodyNode, "height", this.bodyHeight + "px");
-         }
-         this.processContainer(this.widgetsForBody, this.bodyNode);
-         this.bodyNode.appendChild(this.resizeNode);
-         if (this.componentId && !this.resizeDisabled) {
-            // Only make it resizable if a componentId has been provided, otherwise it will not be persisted correctly
-            $(this.bodyNode).resizable({
-               handles: {
-                  "s": this.resizeNode
-               },
-               resize: lang.hitch(this, this.onResize),
-               stop: lang.hitch(this, this.onResizeEnd)
-            });
-         } else {
-            domClass.add(this.domNode, this.baseClass + "--no-resize");
-         }
-      },
-
-      /**
-       * Creates the widgets inside a container
-       *
-       * @instance
-       * @param widgets The widgets to create
-       * @param container The container to place the widgets in
-       */
-      processContainer: function alfresco_dashlets_Dashlet__processContainer(widgets, container) {
-         if (widgets && widgets.length)
-         {
-            this.processWidgets(lang.clone(widgets), container);
-            domClass.add(container, this.baseClass + "__widgets");
-         }
-         else
-         {
-            domClass.remove(container, this.baseClass + "__widgets");
-         }
-      },
-
-      /**
-       * Make some of the i18n keys available for the template
-       *
-       * @instance
-       */
-      postMixInProperties: function alfresco_dashlets_Dashlet__postMixInProperties() {
-         // construct our I18N labels ready for template
-         this.label = {};
-         array.forEach(["title"], lang.hitch(this, function(key) {
-            this.label[key] = this.message(key);
-            if (this.title) {
-               this.label[key] = this.title;
+         /**
+          * Implements the widget life-cycle method to add drag-and-drop upload capabilities to the root DOM node.
+          * This allows files to be dragged and dropped from the operating system directly into the browser
+          * and uploaded to the location represented by the document list.
+          *
+          * @instance
+          */
+         postCreate: function alfresco_dashlets_Dashlet__postCreate() {
+            this.processContainer(this.widgetsForTitleBarActions, this.titleBarActionsNode);
+            this.processContainer(this.widgetsForToolbar, this.toolbarNode);
+            this.processContainer(this.widgetsForToolbar2, this.toolbar2Node);
+            if (this.bodyHeight) {
+               domStyle.set(this.bodyNode, "height", this.bodyHeight + "px");
             }
-         }));
-      },
+            this.processContainer(this.widgetsForBody, this.bodyNode);
+            if (this.componentId && !this.resizeDisabled) {
+               // Only make it resizable if a componentId has been provided, otherwise it will not be persisted correctly
+               $(this.bodyNode).resizable({
+                  handles: {
+                     "s": this.resizeNode
+                  },
+                  resize: lang.hitch(this, this.onResize),
+                  stop: lang.hitch(this, this.onResizeEnd)
+               });
+            } else {
+               domClass.add(this.domNode, this.baseClass + "--no-resize");
+            }
+         },
 
-      /**
-       * 
-       * @instance
-       * @param {object} evt The resize event object
-       * @param {object} ui The data about the resize
-       */
-      onResize: function alfresco_layout_AlfSideBarContainer__onResize(evt, ui) {
-         // Fire a custom event to let contained objects know that the node has been resized.
-         this.alfPublishResizeEvent(this.bodyNode);
-      },
+         /**
+          * Creates the widgets inside a container
+          *
+          * @instance
+          * @param widgets The widgets to create
+          * @param container The container to place the widgets in
+          */
+         processContainer: function alfresco_dashlets_Dashlet__processContainer(widgets, container) {
+            if (widgets && widgets.length) {
+               this.processWidgets(lang.clone(widgets), container);
+               domClass.add(container, this.baseClass + "__widgets");
+            } else {
+               domClass.remove(container, this.baseClass + "__widgets");
+            }
+         },
 
-      /**
-       * Calls [resizeHandler]{@link module:alfresco/layout/AlfSideBarContainer#resizeHandler} and then
-       * saves the new width as a user preference.
-       * 
-       * @instance
-       * @param {object} evt The resize event
-       */
-      onResizeEnd: function alfresco_layout_AlfSideBarContainer__onResizeEnd(evt, ui) {
-         this.onResize(evt, ui);
-         // this.alfPublish(this.setPreferenceTopic, {
-         //    preference: this.sidebarWidthPreferenceId,
-         //    value: this.lastSidebarWidth
-         // });
-         // this.hiddenSidebarWidth = this.lastSidebarWidth;
-      }
+         /**
+          * Make some of the i18n keys available for the template
+          *
+          * @instance
+          */
+         postMixInProperties: function alfresco_dashlets_Dashlet__postMixInProperties() {
+            // construct our I18N labels ready for template
+            this.label = {};
+            array.forEach(["title"], lang.hitch(this, function(key) {
+               this.label[key] = this.message(key);
+               if (this.title) {
+                  this.label[key] = this.title;
+               }
+            }));
+            this.alfSubscribe(this.storeDashletHeightTopic + "_FAILURE", lang.hitch(this, this.onHeightStorageError));
+         },
 
+         /**
+          * Handle errors that occur while storing the dashlet height
+          *
+          * @instance
+          * @param {Object} payload The payload from the publishing of the failure topic
+          */
+         onHeightStorageError: function(payload){
+            this.alfPublish("ALF_DISPLAY_NOTIFICATION", {
+               message: "Error occurred while storing dashlet height"
+            }, true);
+            this.alfLog("error", "Error storing dashlet height:\n\"" + payload.response.message + "\"");
+         },
+
+         /**
+          * 
+          * @instance
+          * @param {object} evt The resize event object
+          * @param {object} ui The data about the resize
+          */
+         onResize: function alfresco_layout_AlfSideBarContainer__onResize( /*jshint unused:false*/ evt, /*jshint unused:false*/ ui) {
+            this.bodyHeight = this.bodyNode.clientHeight - 15; // The bottom-padding that the resize bar lives in
+            this.alfPublishResizeEvent(this.bodyNode);
+         },
+
+         /**
+          * Calls [resizeHandler]{@link module:alfresco/layout/AlfSideBarContainer#resizeHandler} and then
+          * saves the new width as a user preference.
+          * 
+          * @instance
+          * @param {object} evt The resize event
+          * @param {object} ui The data about the resize
+          */
+         onResizeEnd: function alfresco_layout_AlfSideBarContainer__onResizeEnd(evt, ui) {
+            this.onResize(evt, ui);
+            this.alfPublish(this.storeDashletHeightTopic, {
+               componentId: this.componentId,
+               alfResponseTopic: this.pubSubScope + this.storeDashletHeightTopic,
+               height: this.bodyHeight
+            }, true);
+         }
+      });
    });
-});
