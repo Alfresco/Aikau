a69edeea741ae265654bfb41a07738bc149200cb
Martin Doyle <martin.doyle@alfresco.com>
2015-07-08 09:09:36 +0100

    AKU-409: DateTextBox cannot be empty - Update tests

--- aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/forms/controls/DateTextBox.get.js
+++ aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/forms/controls/DateTextBox.get.js
@@ -1,9 +1,4 @@
 model.jsonModel = {
-   publishOnReady: [
-      {
-         publishTopic: "GLOBAL_UPDATE_TOPIC"
-      }
-   ],
    services: [
       {
          name: "alfresco/services/LoggingService",
@@ -18,28 +13,77 @@ model.jsonModel = {
    ],
    widgets: [
       {
-         name: "alfresco/forms/Form",
+         name: "alfresco/layout/HorizontalWidgets",
          config: {
-            id: "FORM",
-            okButtonPublishTopic: "TEST_PUBLISH",
             widgets: [
                {
-                  name: "alfresco/forms/controls/DateTextBox",
+                  name: "alfresco/forms/Form",
+                  id: "VALID_DATES_FORM",
                   config: {
-                     id: "DOJODATETEXTBOX",
-                     name: "someDate",
-                     value: "2012-12-12",
-                     label: "Date"
+                     okButtonPublishTopic: "VALID_DATES_FORM_SUBMIT",
+                     widgets: [
+                        {
+                           name: "alfresco/forms/controls/DateTextBox",
+                           id: "VALID_DATE_VALUE_1",
+                           config: {
+                              name: "validDate1",
+                              value: "2012-12-12",
+                              label: "Valid date #1"
+                           }
+                        },
+                        {
+                           name: "alfresco/forms/controls/DateTextBox",
+                           id: "VALID_DATE_VALUE_2",
+                           config: {
+                              name: "validDate2",
+                              value: "2015-07-07",
+                              label: "Valid date #2"
+                           }
+                        }
+                     ]
+                  }
+               },
+               {
+                  name: "alfresco/forms/Form",
+                  id: "INVALID_DATES_FORM",
+                  config: {
+                     okButtonPublishTopic: "INVALID_DATES_FORM_SUBMIT",
+                     widgets: [
+                        {
+                           name: "alfresco/forms/controls/DateTextBox",
+                           id: "LETTERS_DATE_VALUE",
+                           config: {
+                              name: "lettersDate",
+                              value: "letters",
+                              label: "Letters date"
+                           }
+                        },
+                        {
+                           name: "alfresco/forms/controls/DateTextBox",
+                           id: "NULL_DATE_VALUE",
+                           config: {
+                              name: "nullDate",
+                              value: null,
+                              label: "Null date"
+                           }
+                        },
+                        {
+                           name: "alfresco/forms/controls/DateTextBox",
+                           id: "UNDEFINED_DATE_VALUE",
+                           config: {
+                              name: "undefinedDate",
+                              value: undefined,
+                              label: "Undefined date"
+                           }
+                        }
+                     ]
                   }
                }
             ]
          }
       },
       {
-         name: "alfresco/logging/SubscriptionLog"
-      },
-      {
-         name: "aikauTesting/TestCoverageResults"
+         name: "alfresco/logging/DebugLog"
       }
    ]
 };
