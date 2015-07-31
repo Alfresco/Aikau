c1fbd2640681b7879d4da5ac667bdf83dcf9e74b
Martin Doyle <martin.doyle@alfresco.com>
2015-07-31 17:08:25 +0100

    AKU-458: MultiSelectInput cannot be disabled - Make it able to be disabled

--- aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/forms/controls/MultiSelectInput.get.js
+++ aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/forms/controls/MultiSelectInput.get.js
@@ -1,3 +1,5 @@
+var hrStyle = "border: 1px solid #eee; width: 300px; margin: 25px 15px;";
+
 model.jsonModel = {
    services: [
       {
@@ -58,73 +60,128 @@ model.jsonModel = {
          }
       },
       {
-         id: "FORM1",
-         name: "alfresco/forms/Form",
+         name: "alfresco/html/HR",
          config: {
-            okButtonPublishTopic: "FORM_POST",
-            pubSubScope: "FORM1_",
-            scopeFormControls: false,
-            value: {
-               tags: ["workspace://SpacesStore/06bd4708-8998-47be-a4ea-0f418bc7bb38", "workspace://SpacesStore/d37d7dfa-8410-46be-af6a-5d5880ca478e"]
-            },
+            style: hrStyle
+         }
+      },
+      {
+         name: "alfresco/layout/HorizontalWidgets",
+         config: {
             widgets: [
                {
-                  id: "MULTISELECT_1",
-                  name: "alfresco/forms/controls/MultiSelectInput",
+                  id: "FORM1",
+                  name: "alfresco/forms/Form",
                   config: {
-                     label: "Tags",
-                     name: "tags",
-                     width: "400px",
-                     optionsConfig: {
-                        queryAttribute: "name",
-                        valueAttribute: "nodeRef",
-                        labelAttribute: "name",
-                        publishTopic: "ALF_RETRIEVE_CURRENT_TAGS",
-                        publishPayload: {
-                           resultsProperty: "response.data.items"
-                        },
-                        searchStartsWith: false
+                     okButtonPublishTopic: "FORM_POST",
+                     pubSubScope: "FORM1_",
+                     scopeFormControls: false,
+                     value: {
+                        tags: ["workspace://SpacesStore/06bd4708-8998-47be-a4ea-0f418bc7bb38", "workspace://SpacesStore/d37d7dfa-8410-46be-af6a-5d5880ca478e"]
                      },
-                     requirementConfig: {
-                        initialValue: true
-                     }
+                     widgets: [
+                        {
+                           id: "MULTISELECT_1",
+                           name: "alfresco/forms/controls/MultiSelectInput",
+                           config: {
+                              label: "Tags",
+                              name: "tags",
+                              width: "400px",
+                              optionsConfig: {
+                                 queryAttribute: "name",
+                                 valueAttribute: "nodeRef",
+                                 labelAttribute: "name",
+                                 publishTopic: "ALF_RETRIEVE_CURRENT_TAGS",
+                                 publishPayload: {
+                                    resultsProperty: "response.data.items"
+                                 },
+                                 searchStartsWith: false
+                              },
+                              requirementConfig: {
+                                 initialValue: true
+                              }
+                           }
+                        }
+                     ]
+                  }
+               },
+               {
+                  id: "FORM2",
+                  name: "alfresco/forms/Form",
+                  config: {
+                     okButtonPublishTopic: "FORM2_POST",
+                     pubSubScope: "FORM2_",
+                     scopeFormControls: false,
+                     value: {
+                        categories: ["CAT_01", "CAT_10"]
+                     },
+                     widgets: [
+                        {
+                           id: "MULTISELECT_2",
+                           name: "alfresco/forms/controls/MultiSelectInput",
+                           config: {
+                              label: "Celestial categories",
+                              name: "categories",
+                              width: "310px",
+                              optionsConfig: {
+                                 choiceCanWrap: true,
+                                 choiceMaxWidth: "150px",
+                                 publishTopic: "ALF_RETRIEVE_CELESTIAL_CATEGORIES",
+                                 labelFormat: {
+                                    choice: "{label}",
+                                    result: "{value}: {label}",
+                                    full: "ID={value}, Name={label}"
+                                 },
+                                 searchStartsWith: false
+                              }
+                           }
+                        }
+                     ]
+                  }
+               },
+               {
+                  id: "FORM3",
+                  name: "alfresco/forms/Form",
+                  config: {
+                     okButtonPublishTopic: "FORM_POST",
+                     pubSubScope: "FORM1_",
+                     scopeFormControls: false,
+                     value: {
+                        tags: ["workspace://SpacesStore/06bd4708-8998-47be-a4ea-0f418bc7bb38", "workspace://SpacesStore/d37d7dfa-8410-46be-af6a-5d5880ca478e"]
+                     },
+                     widgets: [
+                        {
+                           id: "MULTISELECT_1",
+                           name: "alfresco/forms/controls/MultiSelectInput",
+                           config: {
+                              label: "Tags (disabled)",
+                              name: "tags",
+                              width: "400px",
+                              optionsConfig: {
+                                 queryAttribute: "name",
+                                 valueAttribute: "nodeRef",
+                                 labelAttribute: "name",
+                                 publishTopic: "ALF_RETRIEVE_CURRENT_TAGS",
+                                 publishPayload: {
+                                    resultsProperty: "response.data.items"
+                                 },
+                                 searchStartsWith: false
+                              },
+                              disablementConfig: {
+                                 initialValue: true
+                              }
+                           }
+                        }
+                     ]
                   }
                }
             ]
          }
       },
       {
-         id: "FORM2",
-         name: "alfresco/forms/Form",
+         name: "alfresco/html/HR",
          config: {
-            okButtonPublishTopic: "FORM2_POST",
-            pubSubScope: "FORM2_",
-            scopeFormControls: false,
-            value: {
-               categories: ["CAT_01", "CAT_10"]
-            },
-            widgets: [
-               {
-                  id: "MULTISELECT_2",
-                  name: "alfresco/forms/controls/MultiSelectInput",
-                  config: {
-                     label: "Celestial categories",
-                     name: "categories",
-                     width: "310px",
-                     optionsConfig: {
-                        choiceCanWrap: true,
-                        choiceMaxWidth: "150px",
-                        publishTopic: "ALF_RETRIEVE_CELESTIAL_CATEGORIES",
-                        labelFormat: {
-                           choice: "{label}",
-                           result: "{value}: {label}",
-                           full: "ID={value}, Name={label}"
-                        },
-                        searchStartsWith: false
-                     }
-                  }
-               }
-            ]
+            style: hrStyle
          }
       },
       {
