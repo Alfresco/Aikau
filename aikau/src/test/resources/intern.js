ce1b995ff7b9ef99845a1d6bd393e1d15564a9d5
Dave Draper <david.draper@alfresco.com>
2015-09-21 21:11:25 +0100

    Increase timeouts to improve test consistency

--- aikau/src/test/resources/intern.js
+++ aikau/src/test/resources/intern.js
@@ -38,11 +38,11 @@ define(["./config/Suites"],
          // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
          // capabilities options specified for an environment will be copied as-is
          environments: [{
-            browserName: "Chrome",
-            chromeOptions: {
-               excludeSwitches: ["ignore-certificate-errors"]
-            }
-         }, {
+            // browserName: "Chrome",
+            // chromeOptions: {
+            //    excludeSwitches: ["ignore-certificate-errors"]
+            // }
+         // }, {
             browserName: "Firefox"
          }],
 
