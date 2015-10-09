bb14676f0bd03a26c3727992faec67209575b1a2
Martin Doyle <martin.doyle@alfresco.com>
2015-10-09 10:58:17 +0100

    AKU-617: Request to join site should return to user dashboard (Also AKU-623) - Update failing-tests section in Suites.js

--- aikau/src/test/resources/config/Suites.js
+++ aikau/src/test/resources/config/Suites.js
@@ -34,11 +34,12 @@ define({
    xbaseFunctionalSuites: [
       "src/test/resources/alfresco/services/SiteServiceTest"
 
-      // THESE SITES REGULARLY FAIL WHEN RUNNING FULL SUITES - INVESTIGATE
+      // THESE SITES REGULARLY, BUT INTERMITTENTLY, FAIL WHEN RUNNING FULL SUITES - INVESTIGATE
       // "src/test/resources/alfresco/dnd/FormCreationTest",
       // "src/test/resources/alfresco/layout/AlfTabContainerTest",
       // "src/test/resources/alfresco/preview/PdfJsPreviewTest"
       // "src/test/resources/alfresco/search/SearchSuggestionsTest",
+      // "src/test/resources/alfresco/services/CloudSyncServiceTest",
    ],
 
    /**
