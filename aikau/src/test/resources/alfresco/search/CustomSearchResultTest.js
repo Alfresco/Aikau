191fe844f47383490df6a5fdaa38f2d7b3ed0c60
Dave Draper <david.draper@alfresco.com>
2015-11-25 17:00:28 +0000

    AKU-706: Finished working through regression failures

--- aikau/src/test/resources/alfresco/search/CustomSearchResultTest.js
+++ aikau/src/test/resources/alfresco/search/CustomSearchResultTest.js
@@ -25,71 +25,71 @@ define(["intern!object",
         "alfresco/TestCommon"],
         function(registerSuite, assert, TestCommon) {
 
-registerSuite(function(){
-   var browser;
+   registerSuite(function(){
+      var browser;
 
-   return {
-      name: "Custom Search Result Tests",
+      return {
+         name: "Custom Search Result Tests",
 
-      setup: function() {
-         browser = this.remote;
-         return TestCommon.loadTestWebScript(this.remote, "/CustomSearchResult", "Custom Search Result Tests").end();
-      },
+         setup: function() {
+            browser = this.remote;
+            return TestCommon.loadTestWebScript(this.remote, "/CustomSearchResult", "Custom Search Result Tests").end();
+         },
 
-      beforeEach: function() {
-         browser.end();
-      },
+         beforeEach: function() {
+            browser.end();
+         },
 
-      "Check that MoreInfo widget is not displayed": function() {
-         // The test page configures the "showMoreInfo" to be false so the MoreInfo renderer should
-         // not be displayed...
-         return browser.findAllByCssSelector(".alfresco-renderers-MoreInfo")
-            .then(function(elements) {
-               assert.lengthOf(elements, 0, "The MoreInfo widget should not have been rendered");
-            });
-      },
+         "Check that MoreInfo widget is not displayed": function() {
+            // The test page configures the "showMoreInfo" to be false so the MoreInfo renderer should
+            // not be displayed...
+            return browser.findAllByCssSelector(".alfresco-renderers-MoreInfo")
+               .then(function(elements) {
+                  assert.lengthOf(elements, 0, "The MoreInfo widget should not have been rendered");
+               });
+         },
 
-      "Check that widgets are rendered above": function() {
-         return browser.findAllByCssSelector(".alfresco-search-AlfSearchResult .aboveCell .alfresco-renderers-Banner")
-            .then(function(elements) {
-               assert.lengthOf(elements, 1, "Additional widget was NOT rendered above the main result properties");
-            });
-      },
+         "Check that widgets are rendered above": function() {
+            return browser.findAllByCssSelector(".alfresco-search-AlfSearchResult .aboveCell .alfresco-renderers-Banner")
+               .then(function(elements) {
+                  assert.lengthOf(elements, 1, "Additional widget was NOT rendered above the main result properties");
+               });
+         },
 
-      "Check that widgets are rendered below": function() {
-         return browser.findAllByCssSelector(".alfresco-search-AlfSearchResult .belowCell .alfresco-renderers-Banner")
-            .then(function(elements) {
-               assert.lengthOf(elements, 1, "Additional widget was NOT rendered below the main result properties");
-            });
-      },
+         "Check that widgets are rendered below": function() {
+            return browser.findAllByCssSelector(".alfresco-search-AlfSearchResult .belowCell .alfresco-renderers-Banner")
+               .then(function(elements) {
+                  assert.lengthOf(elements, 1, "Additional widget was NOT rendered below the main result properties");
+               });
+         },
 
-      "Check that right-click actions are enabled": function() {
-         return browser.findByCssSelector(".alfresco-search-AlfSearchResult .aboveCell .alfresco-renderers-Banner")
-            .moveMouseTo()
-            .clickMouseButton(2)
-         .end()
-         .findAllByCssSelector(".alfresco-menus-AlfContextMenu")
-            .then(function(elements) {
-               assert.lengthOf(elements, 1, "Right-click context actions menu not displayed");
-            });
-      },
+         "Check that right-click actions are enabled": function() {
+            return browser.findByCssSelector(".alfresco-search-AlfSearchResult .aboveCell .alfresco-renderers-Banner")
+               .moveMouseTo()
+               .clickMouseButton(2)
+            .end()
+            .findAllByCssSelector(".alfresco-menus-AlfContextMenu")
+               .then(function(elements) {
+                  assert.lengthOf(elements, 1, "Right-click context actions menu not displayed");
+               });
+         },
 
-      "Check that the additional action filter is applied": function() {
-         return browser.findAllByCssSelector(".alfresco-menus-AlfContextMenu .alfresco-menus-AlfMenuItem")
-            .then(function(elements) {
-               assert.lengthOf(elements, 9, "Not enough actions were displayed");
-            })
-         .end()
-         .findByCssSelector(".alfresco-menus-AlfContextMenu .alfresco-menus-AlfMenuItem:last-child .dijitMenuItemLabel")
-            .getVisibleText()
-            .then(function(text) {
-               assert.equal(text, "Test Action");
-            });
-      },
-      
-      "Post Coverage Results": function() {
-         TestCommon.alfPostCoverageResults(this, browser);
-      }
-   };
+         "Check that the additional action filter is applied": function() {
+            return browser.findAllByCssSelector(".alfresco-menus-AlfContextMenu .alfresco-menus-AlfMenuItem")
+               .then(function(elements) {
+                  assert.lengthOf(elements, 9, "Not enough actions were displayed");
+               })
+            .end()
+            .findByCssSelector(".alfresco-menus-AlfContextMenu .alfresco-menus-AlfMenuItem:last-child .dijitMenuItemLabel")
+               .getVisibleText()
+               .then(function(text) {
+                  assert.equal(text, "Test Action");
+               });
+         },
+         
+         "Post Coverage Results": function() {
+            TestCommon.alfPostCoverageResults(this, browser);
+         }
+      };
    });
 });
