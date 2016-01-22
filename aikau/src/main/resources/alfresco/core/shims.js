b1a953e00db36491a019f3811b6d8b9325630dff
Martin Doyle <martin.doyle@alfresco.com>
2016-01-22 07:37:48 +0000

    AKU-746: Create new UploadService - Add array.reduce shim

--- aikau/src/main/resources/alfresco/core/shims.js
+++ aikau/src/main/resources/alfresco/core/shims.js
@@ -43,9 +43,56 @@ define([], function() {
             return;
          }
          this._addObjectKeys();
+         this._addArrayReduce();
          this._addDateNow();
          this._addTextContent();
          _applied = true;
+      },
+
+      /**
+       * Add a reduce() function to the global Array object, because dojo/_base/array does not support it.
+       * From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce#Polyfill
+       *
+       * @protected
+       * @instance
+       */
+      _addArrayReduce: function alfresco_core_shim___addArrayReduce() {
+         /*jshint bitwise:false,freeze:false,eqnull:true*/
+
+         // Production steps of ECMA-262, Edition 5, 15.4.4.21
+         // Reference: http://es5.github.io/#x15.4.4.21
+         if (!Array.prototype.reduce) {
+            Array.prototype.reduce = function(callback /*, initialValue*/ ) {
+               "use strict";
+               if (this == null) {
+                  throw new TypeError("Array.prototype.reduce called on null or undefined");
+               }
+               if (typeof callback !== "function") {
+                  throw new TypeError(callback + " is not a function");
+               }
+               var t = Object(this),
+                  len = t.length >>> 0,
+                  k = 0,
+                  value;
+               if (arguments.length === 2) {
+                  value = arguments[1];
+               } else {
+                  while (k < len && !(k in t)) {
+                     k++;
+                  }
+                  if (k >= len) {
+                     throw new TypeError("Reduce of empty array with no initial value");
+                  }
+                  value = t[k++];
+               }
+               for (; k < len; k++) {
+                  if (k in t) {
+                     value = callback(value, t[k], k, t);
+                  }
+               }
+               return value;
+            };
+         }
       },
 
       /**
