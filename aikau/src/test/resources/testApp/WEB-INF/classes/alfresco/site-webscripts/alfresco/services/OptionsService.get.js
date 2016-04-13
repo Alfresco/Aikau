8dddb99efb4b6d711357efd7549a8926c8c4d059
Martin Doyle <martin.doyle@alfresco.com>
2016-04-13 11:20:50 +0100

    AKU-926: OptionsService issue - Permit empty strings as itemsAttribute value and update tests

--- aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/services/OptionsService.get.js
+++ aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/services/OptionsService.get.js
@@ -208,7 +208,7 @@ model.jsonModel = {
                            name: "alfresco/forms/controls/MultiSelectInput",
                            config: {
                               fieldId: "MULTI_SELECT_INPUT_3",
-                              label: "Select People",
+                              label: "Select People #1",
                               description: "The people options are provided by the OptionsService, but can be filtered via the ServiceStore. More than one user can be selected",
                               name: "people1",
                               width: "400px",
@@ -232,7 +232,7 @@ model.jsonModel = {
                            name: "alfresco/forms/controls/MultiSelectInput",
                            config: {
                               fieldId: "MULTI_SELECT_INPUT_4",
-                              label: "Select People",
+                              label: "Select People #2",
                               description: "The people options are provided by the UserService, but can be filtered via the ServiceStore. Users are displayed with a sensible name.",
                               name: "people2",
                               width: "400px",
@@ -243,6 +243,30 @@ model.jsonModel = {
                                  publishTopic: "ALF_GET_USERS",
                                  publishPayload: {
                                     resultsProperty: "items"
+                                 }
+                              }
+                           }
+                        },
+                        {
+                           id: "MULTI_SELECT_INPUT_5",
+                           name: "alfresco/forms/controls/MultiSelectInput",
+                           config: {
+                              fieldId: "MULTI_SELECT_INPUT_5",
+                              label: "Select People #3",
+                              description: "This is essentially the same as 'Select People #1' but the payload contains an empty string as the itemsAttribute",
+                              name: "people1",
+                              width: "400px",
+                              optionsConfig: {
+                                 queryAttribute: "label",
+                                 labelAttribute: "label",
+                                 valueAttribute: "value",
+                                 publishTopic: "ALF_GET_FORM_CONTROL_OPTIONS",
+                                 publishPayload: {
+                                    resultsProperty: "options",
+                                    url: url.context + "/proxy/alfresco/api/people/raw",
+                                    itemsAttribute: "",
+                                    labelAttribute: "userName",
+                                    valueAttribute: "userName"
                                  }
                               }
                            }
