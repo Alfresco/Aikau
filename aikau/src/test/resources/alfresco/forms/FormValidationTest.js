14e0ad490ac18c28b71a1105c6ca502fbc559d37
ddraper <david.draper@alfresco.com>
2016-06-22 11:56:09 +0100

    AKU-1002: Support validation of enablement/requirement state change

--- aikau/src/test/resources/alfresco/forms/FormValidationTest.js
+++ aikau/src/test/resources/alfresco/forms/FormValidationTest.js
@@ -22,8 +22,45 @@
  */
 define(["module",
         "alfresco/defineSuite",
-        "intern/chai!assert"],
-        function(module, defineSuite, assert) {
+        "intern/chai!assert",
+        "alfresco/TestCommon"],
+        function(module, defineSuite, assert, TestCommon) {
+
+   var formSelectors = TestCommon.getTestSelectors("alfresco/forms/Form");
+   var textBoxSelectors = TestCommon.getTestSelectors("alfresco/forms/controls/TextBox");
+   var selectSelectors = TestCommon.getTestSelectors("alfresco/forms/controls/Select");
+
+   var selectors = {
+      forms: {
+         noInitialValidation: {
+            confirmationButton: TestCommon.getTestSelector(formSelectors, "confirmation.button", ["NO_INITIAL_VALIDATION"]),
+            disabledConfirmationButton: TestCommon.getTestSelector(formSelectors, "confirmation.button.disabled", ["NO_INITIAL_VALIDATION"])
+         },
+         disablementChanges: {
+            confirmationButton: TestCommon.getTestSelector(formSelectors, "confirmation.button", ["FORM3"]),
+            disabledConfirmationButton: TestCommon.getTestSelector(formSelectors, "confirmation.button.disabled", ["FORM3"])
+         }
+      },
+      textBoxes: {
+         tb1: {
+            input: TestCommon.getTestSelector(textBoxSelectors, "input", ["TB1"])
+         },
+         tb2: {
+            input: TestCommon.getTestSelector(textBoxSelectors, "input", ["TB2"])
+         },
+         tb3: {
+            input: TestCommon.getTestSelector(textBoxSelectors, "input", ["TB3"])
+         },
+         tb5: {
+            input: TestCommon.getTestSelector(textBoxSelectors, "input", ["TB5"])
+         }
+      },
+      select: {
+         firstOption: TestCommon.getTestSelector(selectSelectors, "nth.option.label", ["SELECT", "1"]),
+         openIcon: TestCommon.getTestSelector(selectSelectors, "open.menu.icon", ["SELECT"]),
+         secondOption: TestCommon.getTestSelector(selectSelectors, "nth.option.label", ["SELECT", "2"])
+      }
+   };
 
    defineSuite(module, {
       name: "Form Validation Display Tests",
@@ -60,16 +97,14 @@ define(["module",
       },
 
       "Check that first form confirmation button is disabled": function() {
-         return this.remote.findAllByCssSelector("#NO_INITIAL_VALIDATION .buttons .alfresco-buttons-AlfButton.confirmationButton.dijitButtonDisabled")
-            .then(function(elements) {
-               assert(elements.length === 1, "Confirmation button was not initially disabled");
-            });
+         return this.remote.findByCssSelector(selectors.forms.noInitialValidation.disabledConfirmationButton);
       },
 
       "Check that second form IS displayed as invalid on load": function() {
          return this.remote.findByCssSelector("#TB3.alfresco-forms-controls-BaseFormControl--invalid")
-            .end()
-            .findByCssSelector("#TB4.alfresco-forms-controls-BaseFormControl--invalid");
+         .end()
+      
+         .findByCssSelector("#TB4.alfresco-forms-controls-BaseFormControl--invalid");
       },
 
       "Error indicators are only shown when form is invalid": function() {
@@ -78,21 +113,21 @@ define(["module",
             .then(function(isDisplayed) {
                assert.isFalse(isDisplayed, "Error image was shown on valid form control");
             })
-            .end()
+         .end()
 
          .findByCssSelector("#TB1 .validation-message")
             .isDisplayed()
             .then(function(isDisplayed) {
                assert.isFalse(isDisplayed, "Error message was shown on valid form control");
             })
-            .end()
+         .end()
 
          .findByCssSelector("#TB3 .alfresco-forms-controls-BaseFormControl__validation-error")
             .isDisplayed()
             .then(function(isDisplayed) {
                assert.isTrue(isDisplayed, "Error image was not shown on invalid form control");
             })
-            .end()
+         .end()
 
          .findByCssSelector("#TB3 .validation-message")
             .isDisplayed()
@@ -102,34 +137,61 @@ define(["module",
       },
 
       "Give and remove focus to first text field and verify control is marked as invalid": function() {
-         return this.remote.findByCssSelector("#TB1 .dijitInputContainer input")
+         return this.remote.findByCssSelector(selectors.textBoxes.tb1.input)
             .click()
-            .end()
-            .findByCssSelector("#TB2 .dijitInputContainer input")
+         .end()
+         
+         .findByCssSelector(selectors.textBoxes.tb2.input)
             .click()
-            .end()
-            .findByCssSelector("#TB1.alfresco-forms-controls-BaseFormControl--invalid");
+         .end()
+         
+         .findByCssSelector("#TB1.alfresco-forms-controls-BaseFormControl--invalid");
       },
 
       "Remove focus from second text field and verify control is marked as invalid": function() {
-         return this.remote.findByCssSelector("#TB3 .dijitInputContainer input")
+         return this.remote.findByCssSelector(selectors.textBoxes.tb3.input)
             .click()
-            .end()
-            .findByCssSelector("#TB2.alfresco-forms-controls-BaseFormControl--invalid");
+         .end()
+         
+         .findByCssSelector("#TB2.alfresco-forms-controls-BaseFormControl--invalid");
       },
 
       "Make third text field valid and then remove focus and verify that control is not marked as invalid": function() {
-         return this.remote.findByCssSelector("#TB5 .dijitInputContainer input")
+         return this.remote.findByCssSelector(selectors.textBoxes.tb5.input)
             .clearValue()
             .type("abcde")
-            .end()
-            .findByCssSelector("#TB3 .dijitInputContainer input")
+         .end()
+      
+         .findByCssSelector(selectors.textBoxes.tb3.input)
             .click()
-            .end()
-            .findAllByCssSelector("#TB5.alfresco-forms-controls-BaseFormControl--invalid")
+         .end()
+
+         .findAllByCssSelector("#TB5.alfresco-forms-controls-BaseFormControl--invalid")
             .then(function(elements) {
                assert.lengthOf(elements, 0, "The control should not be marked as invalid (TB5)");
             });
+      },
+
+      "Disablement change form confirmation button is initially enabled": function() {
+         return this.remote.findByCssSelector(selectors.forms.disablementChanges.confirmationButton)
+         .end()
+
+         .findAllByCssSelector(selectors.forms.disablementChanges.disabledConfirmationButton)
+            .then(function(elements) {
+               assert.lengthOf(elements, 0);
+            });
+      },
+
+      "Selecting option to enable text area and make it required should disable form confirmation button": function() {
+         return this.remote.findByCssSelector(selectors.select.openIcon)
+            .click()
+         .end()
+
+         .findByCssSelector(selectors.select.secondOption)
+            .click()
+         .end()
+
+         .findByCssSelector(selectors.forms.disablementChanges.confirmationButton);
       }
    });
 });
