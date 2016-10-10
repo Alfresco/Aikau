f4a051e4bf764a4b97a607006d7d6cb37a74ac3a
ddraper <david.draper@alfresco.com>
2016-10-10 14:27:29 +0100

    AKU-1096: Updated AlfMenuBarToggle tests to current best practices

--- aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/menus/AlfMenuBarToggle.get.js
+++ aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/menus/AlfMenuBarToggle.get.js
@@ -105,11 +105,7 @@ model.jsonModel = {
          }
       },
       {
-         name: "alfresco/logging/SubscriptionLog"
-      },
-      {
-         name: "aikauTesting/TestCoverageResults"
+         name: "alfresco/logging/DebugLog"
       }
    ]
-}
-;
+};
