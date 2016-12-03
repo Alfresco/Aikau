693dfd90cae669cede4bc88eb0878acf8118c6f4
AFaust <axel.faust@acosix.org>
2016-12-03 20:26:11 +0100

    Extend performance test to demonstrate overhead of multiple configuration attributes

--- aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/core/Performance.get.js
+++ aikau/src/test/resources/testApp/WEB-INF/classes/alfresco/site-webscripts/alfresco/core/Performance.get.js
@@ -10,22 +10,33 @@ for (var j=0; j < 100; j++)
    items.push(item);
 }
 
-var rowModel = [];
+var rowModel = [], cellModel, propertyModel;
 for (var i=0; i<50; i++)
 {
-   rowModel.push({
+   propertyModel = {
+      name: "alfresco/renderers/Property",
+      config: {
+         propertyToRender: "p" + i
+      }
+   };
+   cellModel = {
       name: "alfresco/lists/views/layouts/Cell",
       config: {
          widgets: [
-            {
-               name: "alfresco/renderers/Property",
-               config: {
-                  propertyToRender: "p" + i
-               }
-            }
+            propertyModel
          ]
       }
-   });
+   };
+   
+   // simulate a somewhat high number of config properties
+   // non-functional so should only highlight performance issues in config handling
+   for (var j=0; j<50; j++)
+   {
+       propertyModel.config['configKey_' + j] = 'configValue_' + j;
+       cellModel.config['configKey_' + j] = 'configValue_' + j;
+   }
+   
+   rowModel.push(cellModel);
 }
 
 model.jsonModel = {
