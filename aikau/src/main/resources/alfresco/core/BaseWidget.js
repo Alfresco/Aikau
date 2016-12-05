82414db40cd98db60b01ef5f4220d3ad2cf16252
ddraper <david.draper@alfresco.com>
2016-12-05 18:54:41 +0000

    AKU-1145: Update following initial review

--- aikau/src/main/resources/alfresco/core/BaseWidget.js
+++ aikau/src/main/resources/alfresco/core/BaseWidget.js
@@ -34,9 +34,8 @@
 define(["dojo/_base/declare",
         "dijit/_WidgetBase", 
         "dijit/_TemplatedMixin",
-        "dojo/text!./templates/Label.html",
         "alfresco/core/Core"], 
-        function(declare, _WidgetBase, _TemplatedMixin, template, Core) {
+        function(declare, _WidgetBase, _TemplatedMixin, Core) {
    
    return declare([_WidgetBase, _TemplatedMixin, Core], {
 
